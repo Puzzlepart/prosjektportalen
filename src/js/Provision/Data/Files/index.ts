f89bc558c47ef514c4a1013e7e0a2d5d063c2ed2
olemp <olemp@puzzlepart.com>
2017-04-26 17:10:49 +0200

    Minor edits

--- src/js/Provision/Data/Files/index.ts
+++ src/js/Provision/Data/Files/index.ts
@@ -9,7 +9,7 @@ import { IListConfig } from "../Config";
  * @param destUrl Destination web URL
  * @param timeout Timeout (ms)
  */
-export const CopyFiles = (conf: IListConfig, destUrl: string, timeout = 25000): Promise<FileAddResult[]> => new Promise<FileAddResult[]>((resolve, reject) => {
+export const CopyFiles = (conf: IListConfig, destUrl: string, timeout = 25000) => new Promise<FileAddResult[]>((resolve, reject) => {
     Logger.write(`Copying files from '${conf.SourceList}' to '${conf.DestinationLibrary}'.`, LogLevel.Info);
     let srcWeb = new Web(conf.SourceUrl),
         srcList = srcWeb.lists.getByTitle(conf.SourceList),
@@ -33,18 +33,19 @@ export const CopyFiles = (conf: IListConfig, destUrl: string, timeout = 25000): 
                 files.push(i);
             }
         });
-        let createFolders = Promise.all(folders.map(f => {
-            let folderUrl = f.replace(RootFolder.ServerRelativeUrl, "");
-            return destWeb.getFolderByServerRelativeUrl(destLibServerRelUrl).folders.add(`${destLibServerRelUrl}/${folderUrl}`);
+        let createFolders = Promise.all(folders.map(folder => {
+            let folderServerRelUrl = folder.replace(RootFolder.ServerRelativeUrl, "");
+            return destWeb.getFolderByServerRelativeUrl(destLibServerRelUrl).folders.add(`${destLibServerRelUrl}/${folderServerRelUrl}`);
         }));
         createFolders.then(_ => {
             let getFileContents = Promise.all(files.map(({ FileRef, LinkFilename }) => new Promise<{ LinkFilename: string, Blob: Blob }>((_resolve, _reject) => {
                 srcWeb.getFileByServerRelativeUrl(FileRef).getBlob().then(blob => _resolve({ LinkFilename: LinkFilename, Blob: blob }), _reject);
             })));
             getFileContents.then(fileContents => {
-                Promise.all(fileContents.map(fc => new Promise<any>((_resolve) => {
+                let createFiles = Promise.all(fileContents.map(fc => new Promise<any>((_resolve) => {
                     destWeb.getFolderByServerRelativeUrl(destLibServerRelUrl).files.add(fc.LinkFilename, fc.Blob, true).then(_resolve, _resolve);
-                }))).then(resolve, resolve);
+                })));
+                createFiles.then(resolve, resolve);
             }, resolve);
         }, resolve);
     });
