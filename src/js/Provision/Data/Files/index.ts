bfb2c67bdf39678cd9ba78f46953c3c63be39e96
olemp <olemp@puzzlepart.com>
2017-04-26 17:01:37 +0200

    CopyFiles: Removed batching, using param timeout (ms) and fixed a bug with folder creation (Standard documents are not provisioned #10)

--- src/js/Provision/Data/Files/index.ts
+++ src/js/Provision/Data/Files/index.ts
@@ -2,15 +2,14 @@ import { Logger, LogLevel, Web, FileAddResult, default as pnp } from "sp-pnp-js"
 import * as Util from "../../../Util";
 import { IListConfig } from "../Config";
 
-const REJECT_TIMEOUT = 20000;
-
 /**
- * Copy files
+ * Copy files and folders to a destination web
  *
  * @param conf Configuration
  * @param destUrl Destination web URL
+ * @param timeout Timeout (ms)
  */
-export const CopyFiles = (conf: IListConfig, destUrl: string): Promise<FileAddResult[]> => new Promise<FileAddResult[]>((resolve, reject) => {
+export const CopyFiles = (conf: IListConfig, destUrl: string, timeout = 25000): Promise<FileAddResult[]> => new Promise<FileAddResult[]>((resolve, reject) => {
     Logger.write(`Copying files from '${conf.SourceList}' to '${conf.DestinationLibrary}'.`, LogLevel.Info);
     let srcWeb = new Web(conf.SourceUrl),
         srcList = srcWeb.lists.getByTitle(conf.SourceList),
@@ -22,29 +21,26 @@ export const CopyFiles = (conf: IListConfig, destUrl: string): Promise<FileAddRe
     ]).then(([{ RootFolder }, items]) => {
         let folders = [], files = [];
         items.forEach(i => {
-            if (i.Folder) {
+            if (i.Folder && i.Folder.hasOwnProperty("ServerRelativeUrl")) {
                 folders.push(i.Folder.ServerRelativeUrl);
             } else {
                 files.push(i);
             }
         });
-        let createFolders = pnp.sp.createBatch();
-        folders.forEach(f => {
+        let createFolders = Promise.all(folders.map(f => {
             let folderUrl = f.replace(RootFolder.ServerRelativeUrl, "");
-            destWeb.getFolderByServerRelativeUrl(destLibServerRelUrl).folders.inBatch(createFolders).add(`${destLibServerRelUrl}/${folderUrl}`);
-        });
-        createFolders.execute().then(_ => {
-            let gfcBatch = pnp.sp.createBatch();
-            let promises = files.map(({ FileRef, LinkFilename }) => new Promise<{ LinkFilename: string, Blob: Blob }>((_resolve, _reject) => {
-                srcWeb.getFileByServerRelativeUrl(FileRef).inBatch(gfcBatch).getBlob().then(blob => _resolve({ LinkFilename: LinkFilename, Blob: blob }), _reject);
-            }));
-            gfcBatch.execute();
-            Promise.all(promises).then(fileContents => {
+            return destWeb.getFolderByServerRelativeUrl(destLibServerRelUrl).folders.add(`${destLibServerRelUrl}/${folderUrl}`);
+        }));
+        createFolders.then(_ => {
+            let getFileContents = Promise.all(files.map(({ FileRef, LinkFilename }) => new Promise<{ LinkFilename: string, Blob: Blob }>((_resolve, _reject) => {
+                srcWeb.getFileByServerRelativeUrl(FileRef).getBlob().then(blob => _resolve({ LinkFilename: LinkFilename, Blob: blob }), _reject);
+            })));
+            getFileContents.then(fileContents => {
                 Promise.all(fileContents.map(fc => new Promise<any>((_resolve) => {
                     destWeb.getFolderByServerRelativeUrl(destLibServerRelUrl).files.add(fc.LinkFilename, fc.Blob, true).then(_resolve, _resolve);
                 }))).then(resolve, resolve);
             }, resolve);
         }, resolve);
     });
-    window.setTimeout(reject, REJECT_TIMEOUT);
+    window.setTimeout(reject, timeout);
 });
