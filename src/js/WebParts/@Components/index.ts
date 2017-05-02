9450d5dad57e5c09e81cd05bd646b0aa6d6bdecf
olemp <olemp@puzzlepart.com>
2017-05-02 15:29:57 +0200

    @Component: DownloadWorkbookButton

--- src/js/WebParts/@Components/index.ts
+++ src/js/WebParts/@Components/index.ts
@@ -2,6 +2,7 @@ import { default as ChromeTitle } from "./ChromeTitle";
 import { default as SearchBox } from "./SearchBox";
 import { default as ModalLink } from "./ModalLink";
 import { Icon } from "./Icon";
+import { DownloadWorkbookButton } from "./Workbook";
 
 
-export { ChromeTitle, SearchBox, ModalLink, Icon };
+export { ChromeTitle, SearchBox, ModalLink, Icon, DownloadWorkbookButton };
