0cc906e65a5b97d5672cab899ff0f2f387651758
olemp <olemp@puzzlepart.com>
2017-05-03 16:07:45 +0200

    DownloadWorkbook not ready for minified bundle due to cptable is not defined error

--- src/js/WebParts/@Components/index.ts
+++ src/js/WebParts/@Components/index.ts
@@ -2,7 +2,6 @@ import { default as ChromeTitle } from "./ChromeTitle";
 import { default as SearchBox } from "./SearchBox";
 import { default as ModalLink } from "./ModalLink";
 import { Icon } from "./Icon";
-import { DownloadWorkbookButton } from "./Workbook";
 
 
-export { ChromeTitle, SearchBox, ModalLink, Icon, DownloadWorkbookButton };
+export { ChromeTitle, SearchBox, ModalLink, Icon };
