34f8d55a15c43bc13dd94edf85aa441b849a7c1d
olemp <olemp@puzzlepart.com>
2017-05-09 14:23:37 +0200

    Removed unused component SearchBox

--- src/js/WebParts/@Components/index.ts
+++ src/js/WebParts/@Components/index.ts
@@ -1,7 +1,6 @@
-import { default as ChromeTitle } from "./ChromeTitle";
-import { default as SearchBox } from "./SearchBox";
-import { default as ModalLink } from "./ModalLink";
+import ChromeTitle from "./ChromeTitle";
+import ModalLink from "./ModalLink";
 import { Icon } from "./Icon";
 
 
-export { ChromeTitle, SearchBox, ModalLink, Icon };
+export { ChromeTitle, ModalLink, Icon };
