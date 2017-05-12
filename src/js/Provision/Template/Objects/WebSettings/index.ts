7ea45d2541492bdbf3a1678c21c8eabf29dd62d4
olemp <olemp@puzzlepart.com>
2017-05-12 19:22:47 +0200

    SiteLogoUrl

--- src/js/Provision/Template/Objects/WebSettings/index.ts
+++ src/js/Provision/Template/Objects/WebSettings/index.ts
@@ -2,7 +2,7 @@ import { IWebSettings } from "sp-pnp-provisioning/lib/schema";
 
 const WebSettings: IWebSettings = {
     WelcomePage: "SitePages/ProjectHome.aspx",
-    SiteLogo: `${_spPageContextInfo.siteAbsoluteUrl}/SiteAssets/pp/img/ICO-Site-Project-11.png`,
+    SiteLogoUrl: `${_spPageContextInfo.siteAbsoluteUrl}/SiteAssets/pp/img/ICO-Site-Project-11.png`,
 };
 
 export default WebSettings;
