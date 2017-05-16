8cfff5192261d736382a8c7ce99748370cee63d3
olemp <olemp@puzzlepart.com>
2017-05-16 15:59:56 +0200

    ProjectPhases: renderPhases/renderDialog

--- src/js/WebParts/ProjectPhases/index.tsx
+++ src/js/WebParts/ProjectPhases/index.tsx
@@ -2,10 +2,11 @@ import * as React from "react";
 import {
     Spinner,
     SpinnerType,
-} from "office-ui-fabric-react/lib/Spinner";
+    Icon,
+} from "office-ui-fabric-react";
 import ProjectPhase from "./ProjectPhase";
 import ChangePhaseDialog from "./ChangePhaseDialog";
-import { ChangeProjectPhase } from "../../Project";
+import * as Project from "../../Project";
 import * as Util from "../../Util";
 import * as Data from "./Data";
 import IProjectPhasesProps from "./IProjectPhasesProps";
@@ -43,51 +44,81 @@ export default class ProjectPhases extends React.PureComponent<IProjectPhasesPro
         const {
             phases,
             currentPhase,
-            checkListData,
-            changePhase,
         } = this.state;
 
-        let checkListItems = (currentPhase && checkListData && checkListData[currentPhase.Id]) ? checkListData[currentPhase.Id].items : [];
+        const isPhaseSet = currentPhase && currentPhase.Name && currentPhase.Name !== "";
+
         if (phases) {
-            return (<div>
-                <ul>
-                    {phases.map((p, index) => {
-                        let classList = [
-                            index === 0 ? "first-phase" : "",
-                            index === (phases.length - 1) ? "last-phase" : "",
-                            currentPhase && (p.Name === currentPhase.Name) ? "selected" : "",
-                            p.PhaseLevel ? p.PhaseLevel.trim().toLowerCase() : "unknown-phaselevel",
-                        ];
-                        return (<ProjectPhase
-                            key={index}
-                            phase={p}
-                            classList={classList}
-                            checkListData={checkListData[p.Id]}
-                            onChangePhase={this.onChangePhase} />);
-                    })}
-                </ul>
-                {changePhase
-                    && <ChangePhaseDialog
-                        phase={changePhase}
-                        checkListItems={checkListItems}
-                        onConfirmPhaseChange={this.onChangePhase}
-                        hideHandler={this.hideDialog} />}
-                <div className="set-phase-text ms-metadata" hidden={currentPhase && currentPhase.Name && currentPhase.Name !== ""}><i className="ms-Icon ms-Icon--Error" aria-hidden="true"></i> {__("ProjectPhases_PhaseNotSetText")}</div>
-            </div>);
+            return (
+                <div>
+                    {this.renderPhases(this.state)}
+                    {this.renderDialog(this.state)}
+                    <div
+                        className="set-phase-text ms-metadata"
+                        hidden={isPhaseSet}>
+                        <Icon iconName="Error" /> {__("ProjectPhases_PhaseNotSetText")}
+                    </div>
+                </div>
+            );
         } else {
             return <Spinner type={SpinnerType.large} />;
         }
     }
 
     /**
+     * Render phases
+     */
+    private renderPhases = ({ phases, currentPhase, checkListData, changePhase }: IProjectPhasesState) => {
+        return (
+            <ul>
+                {phases.map((p, index) => {
+                    let classList = [
+                        index === 0 ? "first-phase" : "",
+                        index === (phases.length - 1) ? "last-phase" : "",
+                        currentPhase && (p.Name === currentPhase.Name) ? "selected" : "",
+                        p.PhaseLevel ? p.PhaseLevel.trim().toLowerCase() : "unknown-phaselevel",
+                    ];
+                    return (
+                        <ProjectPhase
+                            key={index}
+                            phase={p}
+                            classList={classList}
+                            checkListData={checkListData[p.Id]}
+                            onChangePhase={this.onChangePhase} />
+                    );
+                })}
+            </ul>
+        );
+    }
+
+    /**
+     * Render dialog
+     */
+    private renderDialog = ({ checkListData, currentPhase, changePhase }: IProjectPhasesState) => {
+        const checkListItems = (currentPhase && checkListData && checkListData[currentPhase.Id]) ? checkListData[currentPhase.Id].items : [];
+        if (changePhase) {
+            return (
+                <ChangePhaseDialog
+                    phase={changePhase}
+                    checkListItems={checkListItems}
+                    onConfirmPhaseChange={this.onChangePhase}
+                    hideHandler={this.hideDialog} />
+            );
+        }
+        return null;
+    }
+
+    /**
      * On change phase
+     *
+     * @param phase New phase
      */
     private onChangePhase = (phase): void => {
         if (phase) {
             this.setState({ changePhase: phase });
         } else {
             this.hideDialog();
-            ChangeProjectPhase(this.state.changePhase)
+            Project.ChangeProjectPhase(this.state.changePhase)
                 .then(Util.reloadPage)
                 .catch(Util.reloadPage);
         }
