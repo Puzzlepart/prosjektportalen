c0de570506234af5078843bb1b56fcafa6aa29ca
olemp <olemp@puzzlepart.com>
2017-05-18 17:14:59 +0200

    Gulp task edits

--- .tasks/default.js
+++ .tasks/default.js
@@ -3,8 +3,8 @@ var gulp = require("gulp"),
 
 
 gulp.task("default", (done) => {
-    runSequence("clean", "build:jsonresources", "package", "build:pnp", done)
+    runSequence("clean", "build:jsonresources", "lint:ts", "package", "build:pnp", done)
 });
 gulp.task("default::prod", (done) => {
-    runSequence("clean", "build:jsonresources", "package::prod", "build:pnp", done)
+    runSequence("clean", "build:jsonresources", "lint:ts", "package::prod", "build:pnp", done)
 });
