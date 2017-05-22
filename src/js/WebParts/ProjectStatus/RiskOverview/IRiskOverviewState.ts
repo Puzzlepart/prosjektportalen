e810b087da85cc4312a0c2aea4b3148d83bde4ce
olemp <olemp@puzzlepart.com>
2017-05-22 15:56:37 +0200

    Show risk matrix before/after tiltak/actions is not working in IE #75

---
+++ src/js/WebParts/ProjectStatus/RiskOverview/IRiskOverviewState.ts
@@ -0,0 +1,14 @@
+interface IRiskOverviewState {
+    items: any[];
+    itemsAsHtml?: any[];
+    columns: any[];
+    showPostAction: boolean;
+}
+
+export const RiskOverviewInitialState = {
+    items: null,
+    columns: null,
+    showPostAction: false,
+};;
+
+export default IRiskOverviewState;
