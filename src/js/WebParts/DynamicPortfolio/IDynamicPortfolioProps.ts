a8eddbb966a3b43b30583c492a7a71d121b38b65
olemp <olemp@puzzlepart.com>
2017-05-26 16:10:41 +0200

    IDynamicPortfolioProps.excelExportConfig (#13)

--- src/js/WebParts/DynamicPortfolio/IDynamicPortfolioProps.ts
+++ src/js/WebParts/DynamicPortfolio/IDynamicPortfolioProps.ts
@@ -4,6 +4,14 @@ import {
     DetailsListLayoutMode,
 } from "office-ui-fabric-react/lib/DetailsList";
 
+export interface IDynamicPortfolioExcelExportConfig {
+    fileName: string;
+    sheetName: string;
+    triggerId: string;
+    buttonLabel: string;
+    buttonIcon: string;
+}
+
 interface IDynamicPortfolioProps {
     searchProperty?: string;
     showGroupBy?: boolean;
@@ -12,6 +20,24 @@ interface IDynamicPortfolioProps {
     constrainMode?: ConstrainMode;
     layoutMode?: DetailsListLayoutMode;
     selectionMode?: SelectionMode;
+    excelExportConfig?: IDynamicPortfolioExcelExportConfig;
 }
+
+export const DynamicPortfolioDefaultProps: Partial<IDynamicPortfolioProps> = {
+    searchProperty: "Title",
+    showGroupBy: true,
+    modalHeaderClassName: "ms-font-xxl",
+    projectInfoFilterField: "GtPcPortfolioPage",
+    constrainMode: ConstrainMode.horizontalConstrained,
+    layoutMode: DetailsListLayoutMode.fixedColumns,
+    selectionMode: SelectionMode.none,
+    excelExportConfig: {
+        fileName: "Porteføljeoversikt-2017-04-28-14-46.xlsx",
+        sheetName: "Sheet A",
+        triggerId: "export-workbook",
+        buttonLabel: "Eksporter til Excel",
+        buttonIcon: "ExcelDocument",
+    },
+};
 
 export default IDynamicPortfolioProps;
