3471aa050d8a44b840ea8ab99cc850faa9fbf485
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-06-26 14:01:31 +0200

    CopyItems using array reduce (#206) (#221)

--- src/js/Provision/Data/Items/index.ts
+++ src/js/Provision/Data/Items/index.ts
@@ -1,7 +1,21 @@
 import { IListConfig } from "../Config";
 import IProgressCallback from "../../IProgressCallback";
 import * as Util from "../../../Util";
-import GetDataContext from "./GetDataContext";
+import GetDataContext, { CopyContext } from "./GetDataContext";
+
+/**
+ * Copy a single list item to the destination web
+ *
+ * @param srcItem The source item
+ * @param fields Fields to copy
+ * @param dataCtx Copy context
+ */
+export const CopyItem = (srcItem: SP.ListItem, fields: string[], dataCtx: CopyContext) => new Promise<void>((resolve, reject) => {
+    const destItm = dataCtx.Destination.list.addItem(new SP.ListItemCreationInformation());
+    fields.forEach(fieldName => Util.setItemFieldValue(fieldName, destItm, srcItem.get_item(fieldName), dataCtx.Destination._, dataCtx.Destination.list));
+    destItm.update();
+    dataCtx.Destination._.executeQueryAsync(resolve, resolve);
+});
 
 /**
  * Copies list items to the destination web
@@ -17,14 +31,7 @@ export const CopyItems = (conf: IListConfig, destUrl: string, onProgress: IProgr
         dataCtx.Source._.load(items);
         dataCtx.Source._.executeQueryAsync(() => {
             onProgress(__("ProvisionWeb_CopyListContent"), String.format(__("ProvisionWeb_CopyItems"), items.get_count(), conf.SourceList, conf.DestinationList));
-            items.get_data().forEach(srcItem => {
-                const destItm = dataCtx.Destination.list.addItem(new SP.ListItemCreationInformation());
-                conf.Fields.forEach(fieldName => {
-                    Util.setItemFieldValue(fieldName, destItm, srcItem.get_item(fieldName), dataCtx.Destination._, dataCtx.Destination.list);
-                });
-                destItm.update();
-            });
-            dataCtx.Destination._.executeQueryAsync(resolve, resolve);
+            items.get_data().reduce((chain, srcItem) => chain.then(_ => CopyItem(srcItem, conf.Fields, dataCtx)), Promise.resolve()).then(resolve, resolve);
         }, resolve);
     });
 });
