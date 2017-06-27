c255963b8f7eaf2eb6c2caf6eecbeb6d3887f081
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-06-27 17:13:48 +0200

    Set offset to -100 #242 (#243)

---
+++ src/js/WebParts/ProjectStatus/StatusNavigation/Nodes.ts
@@ -0,0 +1,38 @@
+const Nodes = [{
+    title: __("StatusPage_Heading_Status"),
+    to: "status-section",
+    iconName: "BarChart4",
+    offset: -100,
+},
+{
+    title: __("StatusPage_Heading_ProjectTime"),
+    to: "fremdrift-section",
+    iconName: "DateTime",
+    offset: -100,
+},
+{
+    title: __("StatusPage_Heading_ProjectBudget"),
+    to: "budget-section",
+    iconName: "Money",
+    offset: -100,
+},
+{
+    title: __("StatusPage_Heading_ProjectQuality"),
+    to: "kvalitet-section",
+    iconName: "Product",
+    offset: -100,
+},
+{
+    title: __("StatusPage_Heading_ProjectRisk"),
+    to: "risiko-section",
+    iconName: "Warning",
+    offset: -100,
+},
+{
+    title: __("StatusPage_Heading_ProjectBenefitAchievement"),
+    to: "gevinst-section",
+    iconName: "Trophy",
+    offset: -100,
+}];
+
+export default Nodes;
