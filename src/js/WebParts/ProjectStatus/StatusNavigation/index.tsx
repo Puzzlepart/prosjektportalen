e9eccbcf4d57e78c63d152f99433656e16629e62
olemp <olemp@puzzlepart.com>
2017-06-27 17:13:22 +0200

    Set offset to -100 #242

--- src/js/WebParts/ProjectStatus/StatusNavigation/index.tsx
+++ src/js/WebParts/ProjectStatus/StatusNavigation/index.tsx
@@ -2,45 +2,9 @@ import * as React from "react";
 import { Link } from "react-scroll";
 import { Icon } from "../../@Components";
 import { default as ExportReport } from "../ExportReport";
+import Nodes from "./Nodes";
 
 const StatusNavigation = project => {
-    const nodes = [{
-        title: __("StatusPage_Heading_Status"),
-        to: "status-section",
-        iconName: "BarChart4",
-        offset: 0,
-    },
-    {
-        title: __("StatusPage_Heading_ProjectTime"),
-        to: "fremdrift-section",
-        iconName: "DateTime",
-        offset: 0,
-    },
-    {
-        title: __("StatusPage_Heading_ProjectBudget"),
-        to: "budget-section",
-        iconName: "Money",
-        offset: 0,
-    },
-    {
-        title: __("StatusPage_Heading_ProjectQuality"),
-        to: "kvalitet-section",
-        iconName: "Product",
-        offset: 0,
-    },
-    {
-        title: __("StatusPage_Heading_ProjectRisk"),
-        to: "risiko-section",
-        iconName: "Warning",
-        offset: 0,
-    },
-    {
-        title: __("StatusPage_Heading_ProjectBenefitAchievement"),
-        to: "gevinst-section",
-        iconName: "Trophy",
-        offset: 0,
-    }];
-
     return (
         <div className="ms-Grid nav-status-container">
             <div className="nav-details ms-Grid-row">
@@ -52,7 +16,7 @@ const StatusNavigation = project => {
                 </div>
             </div>
             <div className="nav-links ms-Grid-row">
-                {nodes.map(({ title, to, offset, iconName }, idx) => (
+                {Nodes.map(({ title, to, offset, iconName }, idx) => (
                     <Link
                         key={idx}
                         className="nav-link ms-Grid-col ms-md2"
