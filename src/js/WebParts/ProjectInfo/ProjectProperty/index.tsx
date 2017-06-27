520c162ea5eb724e5e72c418bc0002ceb52b9cc9
olemp <olemp@puzzlepart.com>
2017-06-27 14:09:58 +0200

    Handling truncation of Note and Text #196

--- src/js/WebParts/ProjectInfo/ProjectProperty/index.tsx
+++ src/js/WebParts/ProjectInfo/ProjectProperty/index.tsx
@@ -1,15 +1,26 @@
 import * as React from "react";
+import * as truncateHtml from "truncate-html";
 import IProjectPropertyProps, { ProjectPropertyDefaultProps } from "./IProjectPropertyProps";
+import IProjectPropertyState from "./IProjectPropertyState";
 import ProjectPropertyModel from "./ProjectPropertyModel";
 
-export default class ProjectProperty extends React.Component<IProjectPropertyProps, any> {
+export default class ProjectProperty extends React.Component<IProjectPropertyProps, IProjectPropertyState> {
     public static defaultProps = ProjectPropertyDefaultProps;
+    private shouldTruncate = false;
+
+    constructor(props: IProjectPropertyProps) {
+        super(props);
+        this.state = {
+            truncate: true,
+        };
+    }
 
     public render(): JSX.Element {
         return this._render(this.props, this.state);
     }
 
-    private _render({ model, style, labelSize, valueSize }: IProjectPropertyProps, { }: any): JSX.Element {
+    private _render({ model, style, labelSize, valueSize, truncateLength }: IProjectPropertyProps, { truncate }: IProjectPropertyState): JSX.Element {
+        let value = model.value;
         let labelClassName = ["_label", "ms-fontWeight-semibold"];
         let valueClassName = ["_value"];
         if (labelSize) {
@@ -18,9 +29,19 @@ export default class ProjectProperty extends React.Component<IProjectPropertyPro
         if (valueSize) {
             valueClassName.push(`ms-font-${valueSize}`);
         }
+
+        /**
+         * Truncates Note and Text if truncateLength is specifed in properties
+         */
+        this.shouldTruncate = truncateLength && truncate && Array.contains(["Note", "Text"], model.type);
+        if (this.shouldTruncate) {
+            value = truncateHtml(model.value, truncateLength, {
+                ellipsis: "...",
+            });
+        }
+
         return (
             <div
-                key={model.internalName}
                 className={`${model.internalName} prop`}
                 data-type={model.type}
                 data-required={model.required}
@@ -29,7 +50,14 @@ export default class ProjectProperty extends React.Component<IProjectPropertyPro
                 <div className={labelClassName.join(" ")}>{model.displayName}</div>
                 <div
                     className={valueClassName.join(" ")}
-                    dangerouslySetInnerHTML={{ __html: model.value }}></div>
+                    dangerouslySetInnerHTML={{ __html: value }}></div>
+                <div hidden={!this.shouldTruncate}>
+                    <a
+                        href="#"
+                        onClick={e => this.setState({
+                            truncate: false,
+                        })}>Vis mer</a>
+                </div>
             </div>
         );
     }
