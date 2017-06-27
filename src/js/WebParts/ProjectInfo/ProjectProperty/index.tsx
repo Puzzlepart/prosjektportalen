8321c0be185cf08bf31e51ed19da1091e83b7222
olemp <olemp@puzzlepart.com>
2017-06-27 10:49:29 +0200

    Converted to full React.Component (#196)

--- src/js/WebParts/ProjectInfo/ProjectProperty/index.tsx
+++ src/js/WebParts/ProjectInfo/ProjectProperty/index.tsx
@@ -1,39 +1,38 @@
 import * as React from "react";
-import IProjectPropertyProps from "./IProjectPropertyProps";
+import IProjectPropertyProps, { ProjectPropertyDefaultProps } from "./IProjectPropertyProps";
 import ProjectPropertyModel from "./ProjectPropertyModel";
-import ProjectPropertyDefaultStyle from "./ProjectPropertyDefaultStyle";
 
-/**
- * Project property
- *
- * @param model Property model
- * @param labelSize Size of label
- * @param valueSize Size of value
- */
-const ProjectProperty = ({ model, labelSize, valueSize, style = ProjectPropertyDefaultStyle }: IProjectPropertyProps): JSX.Element => {
-    let labelClassName = ["_label", "ms-fontWeight-semibold"];
-    let valueClassName = ["_value"];
-    if (labelSize) {
-        labelClassName.push(`ms-font-${labelSize}`);
+export default class ProjectProperty extends React.Component<IProjectPropertyProps, any> {
+    public static defaultProps = ProjectPropertyDefaultProps;
+
+    public render(): JSX.Element {
+        return this._render(this.props, this.state);
     }
-    if (valueSize) {
-        valueClassName.push(`ms-font-${valueSize}`);
-    }
-    return (
-        <div
-            key={model.internalName}
-            className={`${model.internalName} prop`}
-            data-type={model.type}
-            data-required={model.required}
-            title={model.description}
-            style={style}>
-            <div className={labelClassName.join(" ")}>{model.displayName}</div>
+
+    private _render({ model, style, labelSize, valueSize }: IProjectPropertyProps, { }: any): JSX.Element {
+        let labelClassName = ["_label", "ms-fontWeight-semibold"];
+        let valueClassName = ["_value"];
+        if (labelSize) {
+            labelClassName.push(`ms-font-${labelSize}`);
+        }
+        if (valueSize) {
+            valueClassName.push(`ms-font-${valueSize}`);
+        }
+        return (
             <div
-                className={valueClassName.join(" ")}
-                dangerouslySetInnerHTML={{ __html: model.value }}></div>
-        </div>
-    );
-};
+                key={model.internalName}
+                className={`${model.internalName} prop`}
+                data-type={model.type}
+                data-required={model.required}
+                title={model.description}
+                style={style}>
+                <div className={labelClassName.join(" ")}>{model.displayName}</div>
+                <div
+                    className={valueClassName.join(" ")}
+                    dangerouslySetInnerHTML={{ __html: model.value }}></div>
+            </div>
+        );
+    }
+}
 
-export default ProjectProperty;
 export { ProjectPropertyModel, IProjectPropertyProps };
