730fd88b218c138923e12134ae06c8d0c8aa6ce9
olemp <olemp@puzzlepart.com>
2017-06-28 13:44:25 +0200

    Added audienceTargeting prop to NewProjectLink

--- src/js/WebParts/NewProjectLink/INewProjectLinkProps.ts
+++ src/js/WebParts/NewProjectLink/INewProjectLinkProps.ts
@@ -1,10 +1,25 @@
+import AudienceTargeting from "../AudienceTargeting";
+
 interface INewProjectLinkProps {
     linkClassName?: string;
     iconProps?: {
         iconName: string,
         style: React.CSSProperties,
     };
+    audienceTargeting?: AudienceTargeting,
 }
+
+export const NewProjectLinkDefaultProps: Partial<INewProjectLinkProps> = {
+    linkClassName: "ms-font-l",
+    iconProps: {
+        iconName: "CirclePlus",
+        style: {
+            verticalAlign: "bottom",
+            marginRight: 5,
+        },
+    },
+    audienceTargeting: AudienceTargeting.Owners,
+};
 
 export default INewProjectLinkProps;
 
