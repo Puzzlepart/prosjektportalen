f3234d8cd2e692b5ea2b763b22c2cb6fc2776e0a
olemp <olemp@puzzlepart.com>
2017-06-28 14:26:26 +0200

    Default action links for ProjectInfo moved to sep. file

--- src/js/WebParts/ProjectInfo/IProjectInfoProps.ts
+++ src/js/WebParts/ProjectInfo/IProjectInfoProps.ts
@@ -1,7 +1,8 @@
 import * as React from "react";
 import ProjectInfoRenderMode from "./ProjectInfoRenderMode";
 import IProjectInfoModalOptions from "./IProjectInfoModalOptions";
-import { IModalLinkProps, ModalLinkIconPosition } from "../@Components/ModalLink";
+import { IModalLinkProps } from "../@Components/ModalLink";
+import ProjectInfoDefaultActionLinks from "./ProjectInfoDefaultActionLinks";
 
 interface IProjectInfoProps {
     showActionLinks?: boolean;
@@ -36,48 +37,7 @@ export const ProjectInfoDefaultProps: Partial<IProjectInfoProps> = {
     welcomePageId: 3,
     renderMode: ProjectInfoRenderMode.Normal,
     containerClassName: "pp-projectInfo",
-    actionLinks: [{
-        url: `${_spPageContextInfo.webAbsoluteUrl}/SitePages/Forms/DispForm.aspx?ID=3`,
-        label: __("ProjectInfo_ViewProperties"),
-        icon: { iconName: "PreviewLink", position: ModalLinkIconPosition.Left },
-        options: {
-            HideContentTypeChoice: true,
-            HideWebPartMaintenancePageLink: true,
-            HideRibbon: true,
-        },
-        reloadOnSubmit: false,
-        showLabel: true,
-    },
-    {
-        url: `${_spPageContextInfo.webAbsoluteUrl}/_layouts/versions.aspx?list=${_spPageContextInfo.pageListId}&ID=${_spPageContextInfo.pageItemId}&FileName=${_spPageContextInfo.serverRequestPath}`,
-        label: __("ProjectInfo_ShowVersionHistory"),
-        icon: { iconName: "History", position: ModalLinkIconPosition.Left },
-        options: {
-            HideRibbon: true,
-        },
-        reloadOnSubmit: false,
-        showLabel: true,
-    },
-    {
-        url: `${_spPageContextInfo.webAbsoluteUrl}/SitePages/Forms/EditForm.aspx?ID=3`,
-        label: __("ProjectInfo_EditProperties"),
-        icon: { iconName: "EditMirrored", position: ModalLinkIconPosition.Left },
-        options: {
-            HideContentTypeChoice: true,
-            HideWebPartMaintenancePageLink: true,
-            HideRibbon: true,
-            HideFormFields: "GtProjectPhase",
-        },
-        reloadOnSubmit: true,
-        showLabel: true,
-    },
-    {
-        url: `${_spPageContextInfo.webAbsoluteUrl}/_layouts/15/prjsetng.aspx`,
-        label: __("ProjectInfo_EditLogo"),
-        icon: { iconName: "AppIconDefault", position: ModalLinkIconPosition.Left },
-        reloadOnSubmit: true,
-        showLabel: true,
-    }],
+    actionLinks: ProjectInfoDefaultActionLinks,
 };
 
 export default IProjectInfoProps;
---
+++ src/js/WebParts/ProjectInfo/ProjectInfoDefaultActionLinks.ts
@@ -0,0 +1,46 @@
+import { IModalLinkProps, ModalLinkIconPosition } from "../@Components/ModalLink";
+
+const ProjectInfoDefaultActionLinks: IModalLinkProps[] = [{
+    url: `${_spPageContextInfo.webAbsoluteUrl}/SitePages/Forms/DispForm.aspx?ID=3`,
+    label: __("ProjectInfo_ViewProperties"),
+    icon: { iconName: "PreviewLink", position: ModalLinkIconPosition.Left },
+    options: {
+        HideContentTypeChoice: true,
+        HideWebPartMaintenancePageLink: true,
+        HideRibbon: true,
+    },
+    reloadOnSubmit: false,
+    showLabel: true,
+},
+{
+    url: `${_spPageContextInfo.webAbsoluteUrl}/_layouts/versions.aspx?list=${_spPageContextInfo.pageListId}&ID=${_spPageContextInfo.pageItemId}&FileName=${_spPageContextInfo.serverRequestPath}`,
+    label: __("ProjectInfo_ShowVersionHistory"),
+    icon: { iconName: "History", position: ModalLinkIconPosition.Left },
+    options: {
+        HideRibbon: true,
+    },
+    reloadOnSubmit: false,
+    showLabel: true,
+},
+{
+    url: `${_spPageContextInfo.webAbsoluteUrl}/SitePages/Forms/EditForm.aspx?ID=3`,
+    label: __("ProjectInfo_EditProperties"),
+    icon: { iconName: "EditMirrored", position: ModalLinkIconPosition.Left },
+    options: {
+        HideContentTypeChoice: true,
+        HideWebPartMaintenancePageLink: true,
+        HideRibbon: true,
+        HideFormFields: "GtProjectPhase",
+    },
+    reloadOnSubmit: true,
+    showLabel: true,
+},
+{
+    url: `${_spPageContextInfo.webAbsoluteUrl}/_layouts/15/prjsetng.aspx`,
+    label: __("ProjectInfo_EditLogo"),
+    icon: { iconName: "AppIconDefault", position: ModalLinkIconPosition.Left },
+    reloadOnSubmit: true,
+    showLabel: true,
+}];
+
+export default ProjectInfoDefaultActionLinks;
