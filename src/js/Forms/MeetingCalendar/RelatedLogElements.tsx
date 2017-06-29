5944a7312c282135a9b193b08c3fc835254674c5
olemp <olemp@puzzlepart.com>
2017-06-29 14:50:13 +0200

    Localized dispFormUrl in MeetingCalendar

--- src/js/Forms/MeetingCalendar/RelatedLogElements.tsx
+++ src/js/Forms/MeetingCalendar/RelatedLogElements.tsx
@@ -1,12 +1,19 @@
 import * as React from "react";
 import { ChromeTitle, ModalLink } from "../../WebParts/@Components";
 
-const LogElement = ({ data: { ID, Title, GtProjectLogDescription } }) => {
-    let dispFormUrl = `../Prosjektlogg/DispForm.aspx?ID=${ID}`;
-    return (<li>
-        <h3><ModalLink label={Title} url={dispFormUrl} options={{ HideRibbon: true }} /></h3>
-        <p className="ms-metadata">{GtProjectLogDescription}</p>
-    </li>);
+const LogElement = ({ data }) => {
+    let dispFormUrl = `${_spPageContextInfo.webAbsoluteUrl}/${__("DefaultView_ProjectLog_Url")}?ID=${data.ID}`.replace("AllItems", "DispForm");
+    return (
+        <li>
+            <h3>
+                <ModalLink
+                    label={data.Title}
+                    url={dispFormUrl}
+                    options={{ HideRibbon: true }} />
+            </h3>
+            <p className="ms-metadata">{data.GtProjectLogDescription}</p>
+        </li>
+    );
 };
 
 const RelatedLogElements = ({ logElements }) => {
