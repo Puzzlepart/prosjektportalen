ed95c1168219f00b17e1144564c2ac1c8471003c
olemp <olemp@puzzlepart.com>
2017-06-29 14:58:57 +0200

    Localization for PhaseChecklist

--- src/js/Forms/PhaseChecklist/index.ts
+++ src/js/Forms/PhaseChecklist/index.ts
@@ -10,10 +10,10 @@ const _: IBaseFormModifications = {
         FormUtil.overridePreSaveAction(() => {
             jQuery(".ms-formvalidation").remove();
             let status = jQuery("select[id*='GtChecklistStatus'] option:selected").text();
-            if (status === "Ignorert" || status === "Ikke relevant") {
+            if (status === __("Choice_GtChecklistStatus_NotRelevant")) {
                 let comment = jQuery("textarea[id*='GtComment']");
                 if (comment.val() === "") {
-                    comment.after(`<div class="ms-formvalidation">Du må angi en kommentar for hvorfor sjekklistepunktet har fått denne statusen.</div>`);
+                    comment.after(`<div class="ms-formvalidation">${__("SiteFields_GtChecklistStatus_FormValidation_NotRelevant")}</div>`);
                     return false;
                 }
             }
