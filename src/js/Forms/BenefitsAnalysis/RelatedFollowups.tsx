dfabcff713164e54f551c2210d61047d2b2334d8
olemp <olemp@puzzlepart.com>
2017-06-29 14:45:35 +0200

    Minor edit

--- src/js/Forms/BenefitsAnalysis/RelatedFollowups.tsx
+++ src/js/Forms/BenefitsAnalysis/RelatedFollowups.tsx
@@ -4,11 +4,20 @@ import { ChromeTitle, ModalLink } from "../../WebParts/@Components";
 
 const FollowupElement = ({ data: { ID, GtMeasurementDate: Date, GtMeasurementComment: Comment, GtMeasurementValue: Value } }) => {
     let dispFormUrl = `../../${__("DefaultView_BenefitsFollowup_Url")}?ID=${ID}`.replace("AllItems", "DispForm");
-    return (<li>
-        <h3><ModalLink label={Util.dateFormat(Date, "LL")} url={dispFormUrl} options={{ HideRibbon: true }} /></h3>
+    return (
+    <li>
+        <h3>
+            <ModalLink 
+            label={Util.dateFormat(Date, "LL")} 
+            url={dispFormUrl}
+             options={{ HideRibbon: true }} />
+            </h3>
         <p className="ms-metadata"><b>{__("SiteFields_GtMeasurementValue_DisplayName")}:</b> {Value}</p>
-        <p className="ms-metadata" hidden={!Comment}><b>{__("String_Comment")}:</b> {Comment}</p>
-    </li>);
+        <p 
+        className="ms-metadata" 
+        hidden={!Comment}><b>{__("String_Comment")}:</b> {Comment}</p>
+    </li>
+    );
 };
 
 export const RelatedFollowups = ({ followups }) => {
