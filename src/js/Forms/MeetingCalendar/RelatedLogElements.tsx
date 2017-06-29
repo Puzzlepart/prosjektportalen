9f976516318b34be6a0f28745a0a60014a9c47db
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-06-29 15:05:31 +0200

    #166 (#268)
    * Minor edit
    
    * Minor syntax fixes
    
    * Localized dispFormUrl in MeetingCalendar
    
    * Localization for RelatedLogElements
    
    * Localization for PhaseChecklist

--- src/js/Forms/MeetingCalendar/RelatedLogElements.tsx
+++ src/js/Forms/MeetingCalendar/RelatedLogElements.tsx
@@ -1,21 +1,34 @@
 import * as React from "react";
 import { ChromeTitle, ModalLink } from "../../WebParts/@Components";
 
-const LogElement = ({ data: { ID, Title, GtProjectLogDescription } }) => {
-    let dispFormUrl = `../Prosjektlogg/DispForm.aspx?ID=${ID}`;
-    return (<li>
-        <h3><ModalLink label={Title} url={dispFormUrl} options={{ HideRibbon: true }} /></h3>
-        <p className="ms-metadata">{GtProjectLogDescription}</p>
-    </li>);
+const LogElement = ({ data }) => {
+    let dispFormUrl = `${_spPageContextInfo.webAbsoluteUrl}/${__("DefaultView_ProjectLog_Url")}?ID=${data.ID}`.replace("AllItems", "DispForm");
+    return (
+        <li>
+            <h3>
+                <ModalLink
+                    label={data.Title}
+                    url={dispFormUrl}
+                    options={{ HideRibbon: true }} />
+            </h3>
+            <p className="ms-metadata">{data.GtProjectLogDescription}</p>
+        </li>
+    );
 };
 
 const RelatedLogElements = ({ logElements }) => {
-    return (<div className="container" style={{ marginTop: "25px" }}>
-        <ChromeTitle title="Relaterte loggelementer" />
-        <ul className="pp-simpleList" style={{ width: "300px" }}>
-            {logElements.map(e => <LogElement data={e} />)}
-        </ul>
-    </div>);
+    return (
+        <div
+            className="container"
+            style={{ marginTop: "25px" }}>
+            <ChromeTitle title={__("WebPart_RelatedLogElements_Title")} />
+            <ul
+                className="pp-simpleList"
+                style={{ width: "300px" }}>
+                {logElements.map(e => <LogElement data={e} />)}
+            </ul>
+        </div>
+    );
 };
 
 export default RelatedLogElements;
