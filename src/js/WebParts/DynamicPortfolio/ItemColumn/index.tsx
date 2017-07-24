b5197b405c5888e17e64d970b2ab7b17ad0de200
olemp <olemp@puzzlepart.com>
2017-07-24 11:54:22 +0200

    Include it as a clickable icon next to the title #273

--- src/js/WebParts/DynamicPortfolio/ItemColumn/index.tsx
+++ src/js/WebParts/DynamicPortfolio/ItemColumn/index.tsx
@@ -15,22 +15,26 @@ import { GetStatusProperties } from "../../ProjectStatus/Utils";
 const _onRenderItemColumn = (item: any, index: number, column: IColumnConfig, titleOnClick: (evt: any) => void): JSX.Element => {
     const columnValue = item[column.key];
 
-    if (!columnValue) {
-        return (
-            <span>
-            </span>
-        );
-    }
-
-    if (column.key === "Title") {
-        return (
-            <a
-                href="#"
-                onClick={titleOnClick}>{columnValue}</a>
-        );
-    }
-    if (column.key === "Path" || column.key === "URL") {
-        return <a href={item.Path}>{columnValue}</a>;
+    switch (column.key) {
+        case "LinkProjectStatus": {
+            console.log(item.Path);
+            return (
+                <a href={`${item.Path}/SitePages/ProjectStatus.aspx`}>
+                    <Icon iconName="BarChart4" />
+                </a>
+            );
+        }
+        case "Title": {
+            return (
+                <a
+                    href="#"
+                    onClick={titleOnClick}>{columnValue}</a>
+            );
+        }
+        case "Path":
+        case "URL": {
+            return <a href={item.Path}>{columnValue}</a>;
+        }
     }
     switch (column.render) {
         case "Date": {
@@ -71,6 +75,11 @@ const _onRenderItemColumn = (item: any, index: number, column: IColumnConfig, ti
             );
         }
     }
+
+    return (
+        <span>
+        </span>
+    );
 };
 
 export default _onRenderItemColumn;
