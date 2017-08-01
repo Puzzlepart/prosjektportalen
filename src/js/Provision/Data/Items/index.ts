bd010ad9ba145a1996b60b9c413c27eb314b266e
olemp <olemp@puzzlepart.com>
2017-08-01 16:11:28 +0200

    Initial commit #283

--- src/js/Provision/Data/Items/index.ts
+++ src/js/Provision/Data/Items/index.ts
@@ -3,6 +3,8 @@ import IProgressCallback from "../../IProgressCallback";
 import * as Util from "../../../Util";
 import GetDataContext, { CopyContext } from "./GetDataContext";
 
+let __SRC_ITEMS = [];
+
 /**
  * Copy a single list item to the destination web
  *
@@ -14,7 +16,16 @@ export const CopyItem = (srcItem: SP.ListItem, fields: string[], dataCtx: CopyCo
     const destItm = dataCtx.Destination.list.addItem(new SP.ListItemCreationInformation());
     fields.forEach(fieldName => Util.setItemFieldValue(fieldName, destItm, srcItem.get_item(fieldName), dataCtx.Destination._, dataCtx.Destination.list));
     destItm.update();
-    dataCtx.Destination._.executeQueryAsync(resolve, resolve);
+    dataCtx.Destination._.load(destItm);
+    dataCtx.Destination._.executeQueryAsync(() => {
+        __SRC_ITEMS.push({
+            SourceId: srcItem.get_fieldValues()["ID"],
+            DestId: destItm.get_fieldValues()["ID"],
+            DestItem: destItm,
+            ParentID: srcItem.get_fieldValues()["ParentID"] ? parseInt(srcItem.get_fieldValues()["ParentID"].get_lookupValue(), 10) : null,
+        });
+        resolve();
+    }, resolve);
 });
 
 /**
@@ -31,7 +42,20 @@ export const CopyItems = (conf: IListConfig, destUrl: string, onProgress: IProgr
         dataCtx.Source._.load(items);
         dataCtx.Source._.executeQueryAsync(() => {
             onProgress(__("ProvisionWeb_CopyListContent"), String.format(__("ProvisionWeb_CopyItems"), items.get_count(), conf.SourceList, conf.DestinationList));
-            items.get_data().reduce((chain, srcItem) => chain.then(_ => CopyItem(srcItem, conf.Fields, dataCtx)), Promise.resolve()).then(resolve, resolve);
+            items.get_data().reduce((chain, srcItem) => chain.then(_ => CopyItem(srcItem, conf.Fields, dataCtx)), Promise.resolve())
+                .then(() => {
+                    __SRC_ITEMS
+                        .filter(item => item.ParentID)
+                        .forEach(item => {
+                            let [parent] = __SRC_ITEMS.filter(({ SourceId }) => SourceId === item.ParentID);
+                            if (parent) {
+                                item.DestItem.set_item("ParentID", parent.DestId);
+                                item.update();
+                            }
+                        });
+                    dataCtx.Destination._.executeQueryAsync(resolve, resolve);
+                })
+                .catch(resolve);
         }, resolve);
     });
 });
