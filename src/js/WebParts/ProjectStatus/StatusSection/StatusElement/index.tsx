b6d578583560246b387b369b40ee2282e45dc579
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-08-15 08:52:14 +0200

    #303 (#312)
    * Navigation color
    
    * Changes to .nav-link to make it work down to approx 1220px width
    
    * Decreased font-size for status-details, h1=>h2, h2=>h3
    
    * ProjectStatus\ExportReport\saveReport(), start fileName with dateDisplay

--- src/js/WebParts/ProjectStatus/StatusSection/StatusElement/index.tsx
+++ src/js/WebParts/ProjectStatus/StatusSection/StatusElement/index.tsx
@@ -18,14 +18,19 @@ export interface IStatusElementProps {
 const StatusElement = ({ name, iconName, statusValue, comment, scrollTo, fieldName }: IStatusElementProps) => {
     let statusCssClass = fieldName ? GetStatusCssClass(fieldName, statusValue) : "";
     return (
-        <Link className="status-element ms-Grid-row" to={scrollTo} offset={-100} smooth={true} duration={300}>
+        <Link
+            className="status-element ms-Grid-row"
+            to={scrollTo}
+            offset={-100}
+            smooth={true}
+            duration={300}>
             <div className="status-icons ms-Grid-col ms-sm12 ms-md2 ms-lg2">
                 <Icon name={iconName} className={statusCssClass} />
             </div>
             <div className="status-details ms-Grid-col ms-sm12 ms-md10 ms-lg8">
-                <h2>{name}</h2>
-                <h1>{statusValue ? statusValue : ""}</h1>
-                {<p>{comment}</p>}
+                <h3>{name}</h3>
+                <h2>{statusValue ? statusValue : ""}</h2>
+                <p>{comment}</p>
             </div>
         </Link>
     );
