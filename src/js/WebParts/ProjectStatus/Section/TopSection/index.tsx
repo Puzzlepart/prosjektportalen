8b6ff281b9734304f56ae1dc5352b7cb01a67c04
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-08-16 13:22:44 +0200

    Moved overall status

--- src/js/WebParts/ProjectStatus/Section/TopSection/index.tsx
+++ src/js/WebParts/ProjectStatus/Section/TopSection/index.tsx
@@ -6,52 +6,52 @@ import ISectionProps from "./ISectionProps";
 
 const TopSection = ({ project, sections }: ISectionProps) => {
     return (
-        <Element name="status-section" className="status-section section ms-Grid-row">
-            <div id="status-section">
-                <div className="ms-Grid-col ms-lg12 ms-xl4 status-project-data status-elements-container">
-                    <div className="status-elements">
-                        <StatusElement
-                            name={__("ProjectStatus_Heading_OverallStatus")}
-                            iconName="BarChart4"
-                            scrollTo="status-section"
-                            statusValue=""
-                            comment={project.GtOverallStatus}
-                            fieldName="GtOverallStatus" />
-                        <div className="status-element ms-Grid-row">
-                            <div className="status-icons ms-Grid-col ms-sm12 ms-md2 ms-lg2">
-                                <i className="ms-Icon ms-Icon--CustomList no-status"></i>
-                            </div>
-                            <div className="status-details ms-Grid-col ms-sm12 ms-md10 ms-lg8">
-                                <h2>{__("ProjectStatus_Heading_ProjectMetadata")}</h2><h1></h1>
-                                <ProjectInfo
-                                    hideChrome={true}
-                                    showActionLinks={false}
-                                    showMissingPropsWarning={false}
-                                    filterField="GtPcProjectStatus"
-                                    labelSize="m"
-                                    valueSize="s" />
-                            </div>
+        <Element
+            name="status-section"
+            className="status-section section ms-Grid-row">
+            <div className="ms-Grid-col ms-lg12 ms-xl4 status-project-data status-elements-container">
+                <div className="status-elements">
+                    <div className="status-element">
+                        <div className="status-icons ms-Grid-col ms-sm12 ms-md2 ms-lg2">
+                            <i className="ms-Icon ms-Icon--CustomList no-status"></i>
+                        </div>
+                        <div className="status-details ms-Grid-col ms-sm12 ms-md10 ms-lg8">
+                            <h2>{__("ProjectStatus_Heading_ProjectMetadata")}</h2><h1></h1>
+                            <ProjectInfo
+                                hideChrome={true}
+                                showActionLinks={false}
+                                showMissingPropsWarning={false}
+                                filterField="GtPcProjectStatus"
+                                labelSize="m"
+                                valueSize="s" />
                         </div>
                     </div>
                 </div>
-                <div className="ms-Grid-col ms-lg12 ms-xl8 status-elements-container">
-                    <div className="status-elements">
-                        {sections.map(({ name, iconName, fieldName }, key) => {
-                            const props = {
-                                key,
-                                name,
-                                iconName,
-                                statusValue: project[fieldName],
-                                comment: project[`${fieldName}Comment`],
-                                fieldName,
-                            };
-                            return (
-                                <StatusElement
-                                    { ...props }
-                                    scrollTo={`section-${key}`} />
-                            );
-                        })}
-                    </div>
+            </div>
+            <div className="ms-Grid-col ms-lg12 ms-xl8 status-elements-container">
+                <div className="status-elements">
+                    <StatusElement
+                        name={__("ProjectStatus_Heading_OverallStatus")}
+                        iconName="BarChart4"
+                        scrollTo="status-section"
+                        statusValue=""
+                        comment={project.GtOverallStatus}
+                        fieldName="GtOverallStatus" />
+                    {sections.map(({ name, iconName, fieldName }, key) => {
+                        const props = {
+                            key,
+                            name,
+                            iconName,
+                            statusValue: project[fieldName],
+                            comment: project[`${fieldName}Comment`],
+                            fieldName,
+                        };
+                        return (
+                            <StatusElement
+                                { ...props }
+                                scrollTo={`section-${key}`} />
+                        );
+                    })}
                 </div>
             </div>
         </Element>
