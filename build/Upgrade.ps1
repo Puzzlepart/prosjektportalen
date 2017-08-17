ddd689b81fae8a8f4c2634a35f60a91cfe90cba3
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-08-17 13:31:06 +0200

    Initial commit

--- build/Upgrade.ps1
+++ build/Upgrade.ps1
@@ -37,6 +37,13 @@ if (-not $GenericCredential -and -not $UseWebLogin.IsPresent) {
     $Credential = $GenericCredential
 }
 
+function Get-WebLanguage($ctx) {
+    $web = $ctx.Web
+    $ctx.Load($web)
+    $ctx.ExecuteQuery()
+    return $web.Language
+}
+
 
 function Connect-SharePoint ($Url) {
     if ($UseWebLogin.IsPresent) {
@@ -50,7 +57,9 @@ function Connect-SharePoint ($Url) {
 
 Connect-SharePoint -Url $Url
 $CurrentVersion = [Version](Get-PnPPropertyBag -Key pp_version)
-$InstallVersion = [Version]("[version]")
+
+# [version] will be replaced with the actual version by 'gulp release'
+$InstallVersion = [Version]("2.1.0")
 
 if($InstallVersion -gt $CurrentVersion) {
     Write-Host "############################################################################" -ForegroundColor Green
@@ -62,7 +71,18 @@ if($InstallVersion -gt $CurrentVersion) {
     Write-Host "" -ForegroundColor Green
     Write-Host "############################################################################" -ForegroundColor Green
 
-    .\Install.ps1 -Url $Url -Environment $Environment -Upgrade -SkipData -SkipDefaultConfig -SkipTaxonomy
+    # .\Install.ps1 -Url $Url -Environment $Environment -Upgrade -SkipData -SkipDefaultConfig -SkipTaxonomy
+
+    
+    Connect-SharePoint $Url        
+    Write-Host "Deploying upgrade packages" -ForegroundColor Green -NoNewLine
+    $Language = Get-WebLanguage -ctx (Get-PnPContext)   
+    $upgradePkgs = Get-ChildItem "./upgrade/$($InstallVersion)/$($Language)/*.pnp" 
+    foreach($pkg in $upgradePkgs) {
+        Apply-PnPProvisioningTemplate $extension.FullName
+    }
+    Write-Host "DONE" -ForegroundColor Green
+    Disconnect-PnPOnline
 } else {    
     Write-Host "You're already on the same or newer version of Project Portal" -ForegroundColor Yellow
 }
