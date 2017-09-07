f0d6986c68984555526c3ecaf3bcb0ceafe347d4
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-09-07 13:42:48 +0200

    HandleQueryParams: added more verbose logging

--- src/js/Forms/HandleQueryParams.ts
+++ src/js/Forms/HandleQueryParams.ts
@@ -1,5 +1,8 @@
 import * as  jQuery from "jquery";
-import { Logger, LogLevel } from "sp-pnp-js";
+import {
+    Logger,
+    LogLevel,
+} from "sp-pnp-js";
 import * as FormUtils from "./Util";
 
 /**
@@ -9,18 +12,24 @@ const HandleQueryParams = () => {
     Logger.log({ message: "Forms: HandleQueryParams", level: LogLevel.Info, data: { queryParams: FormUtils.getQueryParams() } });
     let { HideContentTypeChoice, HideFormFields, HideWebPartMaintenancePageLink, HideAddNew, HideViewSelector } = FormUtils.getQueryParams();
     if (HideContentTypeChoice === "1") {
+        Logger.log({ message: "HideContentTypeChoice specified in URL, hiding Content Type choice", level: LogLevel.Info, data: {} });
         FormUtils.hideContentTypeChoice();
     }
     if (HideWebPartMaintenancePageLink === "1") {
+        Logger.log({ message: "HideWebPartMaintenancePageLink specified in URL, hiding WebPartMaintenancePageLink", level: LogLevel.Info, data: {} });
         jQuery("a[id*='WebPartMaintenancePageLink']").parents("table").first().hide();
     }
     if (HideFormFields) {
-        HideFormFields.split(",").forEach(fName => FormUtils.hideFormField(fName));
+        const fieldsArray = HideFormFields.split(",");
+        Logger.log({ message: "HideFormFields specified in URL, hiding fields", level: LogLevel.Info, data: { fieldsArray } });
+        fieldsArray.forEach(fName => FormUtils.hideFormField(fName));
     }
     if (HideAddNew) {
+        Logger.log({ message: "HideAddNew specified in URL, hiding .ms-list-addnew", level: LogLevel.Info, data: {} });
         jQuery(".ms-list-addnew").hide();
     }
     if (HideViewSelector) {
+        Logger.log({ message: "HideViewSelector specified in URL, hiding .ms-csrlistview-controldiv", level: LogLevel.Info, data: {} });
         jQuery(".ms-csrlistview-controldiv").hide();
     }
 };
