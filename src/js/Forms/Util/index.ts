ef527697f074c04954a5dcf77cf685bcba444736
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-09-13 15:21:48 +0200

    Some syntax fixes and comments

--- src/js/Forms/Util/index.ts
+++ src/js/Forms/Util/index.ts
@@ -22,7 +22,7 @@ export const getQueryParams = (): IQueryParams => {
 /**
  * Hides field in form
  *
- * @param fieldName Field name
+ * @param {string} fieldName Field name
  */
 export const hideFormField = (fieldName: string): void => {
     const $fieldRow = jQuery(`input[id*='${fieldName}_'], select[id*='${fieldName}_'], div[id*='${fieldName}_']`).parents("tr").first();
@@ -40,8 +40,8 @@ export const hideContentTypeChoice = (): void => {
 /**
  * Inserts form container with the specified ID
  *
- * @param id ID of form container
- * @param refNodeId Reference node ID
+ * @param {string} id ID of form container
+ * @param {string} refNodeId Reference node ID
  */
 export const insertFormContainer = (id: string, refNodeId = "WebPartWPQ1"): HTMLElement => {
     let refNode = document.getElementById(refNodeId),
@@ -54,9 +54,36 @@ export const insertFormContainer = (id: string, refNodeId = "WebPartWPQ1"): HTML
 /**
  * Overrides pre save action with the specified function
  *
- * @param func Override function
+ * @param {Function} func Override function
  */
 export const overridePreSaveAction = (func): void => {
     let f = "PreSaveAction";
     window[f] = func;
+};
+
+
+/**
+* Creates a custom save button which awaits a number of promises before it triggers
+* the original click event.
+*
+* @param {any} promise Promise we want to wait for before saving
+* @param {string} saveBtnSelector jQuery selector for save button
+*/
+export const CreateCallbackEnabledSaveButton = (promise: () => Promise<boolean>, saveBtnSelector = "input[id$='SaveItem']"): void => {
+    const originalSaveBtn = jQuery(saveBtnSelector),
+        customSaveBtn = jQuery("<input />")
+            .attr("type", "button")
+            .val(originalSaveBtn.val())
+            .click(() => {
+                promise()
+                    .then(shouldSave => {
+                        if (shouldSave) {
+                            window.setTimeout(() => {
+                                originalSaveBtn.trigger("click");
+                            }, 1);
+                        }
+                    });
+            });
+    customSaveBtn.insertBefore(originalSaveBtn);
+    originalSaveBtn.hide();
 };
