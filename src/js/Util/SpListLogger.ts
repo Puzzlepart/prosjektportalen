45457283f42ba809bd74a67be9dd7ed81899eacd
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-09-14 13:56:53 +0200

    SpListLogger: Added support for LogLevel

--- src/js/Util/SpListLogger.ts
+++ src/js/Util/SpListLogger.ts
@@ -1,11 +1,16 @@
-import { Site } from "sp-pnp-js";
+import {
+    Site,
+    LogLevel,
+} from "sp-pnp-js";
 
 interface ILogEntry {
-    Title: string;
+    Message: string;
+    LogLevel: LogLevel;
 }
 
-
-
+/**
+ * SharePoint List logger
+ */
 export default class SpListLogger {
     public List: string;
     private SiteUrl: string;
@@ -23,10 +28,37 @@ export default class SpListLogger {
     /**
      * Logs an entry to the SP list
      *
-     * @param {ILogElement} entry Log entry
+     * @param {ILogEntry} entry Log entry
      */
     public log(entry: ILogEntry): void {
         const spList = new Site(this.SiteUrl).rootWeb.lists.getByTitle(this.List);
-        spList.items.add(entry);
+        spList.items.add({
+            ...entry,
+            LogLevel: this.getLogLevelString(entry),
+        });
+    }
+
+    /**
+     * Get log level string representation
+     *
+     * @param {entry} entry Log entry
+     */
+    private getLogLevelString(entry: ILogEntry): string {
+        switch (entry.LogLevel) {
+            case LogLevel.Error: {
+                return __("String_LogLevel_Error");
+            }
+            case LogLevel.Info: {
+                return __("String_LogLevel_Info");
+            }
+            case LogLevel.Warning: {
+                return __("String_LogLevel_Warning");
+            }
+            default: {
+                return "";
+            }
+        }
     }
 }
+
+export { LogLevel };
