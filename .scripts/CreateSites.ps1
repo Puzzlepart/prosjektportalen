b1979689cc171f0ab97c0907cd8890d32a024eba
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-09-15 10:09:05 +0200

    Update to CreateSites.ps1 script

--- .scripts/CreateSites.ps1
+++ .scripts/CreateSites.ps1
@@ -6,12 +6,16 @@ Param(
     [Parameter(Mandatory = $true)]
     [int]$Count,
     [Parameter(Mandatory = $false)]
-    [int]$Lcid = 1044
+    [string]$Title = "Prosjektportalen",
+    [Parameter(Mandatory = $false)]
+    [int]$Lcid = 1044,
+    [Parameter(Mandatory = $false)]
+    [string]$GenericCredential
 )
 
-Connect-PnPOnline $Url.Replace(".sharepoint", "-admin.sharepoint")
+Connect-PnPOnline $Url.Replace(".sharepoint", "-admin.sharepoint") -Credentials $GenericCredential
 
 for ($i = 1; $i -le $Count; $i++) {
     $index = ("{0:D2}" -f $i)
-    New-PnPTenantSite -Title Prosjektportalen -Url "$($Url)/sites/$($Prefix)-$($index)" -Owner olemp@olemp.onmicrosoft.com -Lcid $Lcid -Template "STS#0" -TimeZone 13 -StorageQuota 10
+    New-PnPTenantSite -Title $Title -Url "$($Url)/sites/$($Prefix)-$($index)" -Owner olemp@olemp.onmicrosoft.com -Lcid $Lcid -Template "STS#0" -TimeZone 13 -StorageQuota 10
 }
