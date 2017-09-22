0fbc088fc24686868ff36d00507d0b7ba58833e1
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-09-22 16:47:06 +0200

    Using async-await in ProvisionWeb

--- src/js/Provision/index.ts
+++ src/js/Provision/index.ts
@@ -8,7 +8,7 @@ import { CopyDefaultData } from "./Data";
 import { ApplyProvisioningTemplate } from "./Template";
 import { ApplyExtensions } from "./Extensions";
 import { GetAllProperties } from "../Util/PropertyBag";
-import SpListLogger, { LogLevel } from "../Util/SpListLogger";
+import SpListLogger, { LogLevel, ILogEntry } from "../Util/SpListLogger";
 import IProgressCallback from "./IProgressCallback";
 
 const __listLogger = new SpListLogger();
@@ -18,31 +18,28 @@ const __listLogger = new SpListLogger();
  * Provisions a project web
  *
  * @param {IProjectModel} model The project model
- * @param {IProgressCallback} onUpdateProgress Progress callback function
+ * @param {IProgressCallback} progressCallbackFunc Progress callback function
  *
  * @returns {string} Redirect URL
  */
-async function ProvisionWeb(project: IProjectModel, onUpdateProgress: IProgressCallback): Promise<string> {
+async function ProvisionWeb(project: IProjectModel, progressCallbackFunc: IProgressCallback): Promise<string> {
     try {
-        onUpdateProgress(RESOURCE_MANAGER.getResource("ProvisionWeb_CreatingWeb"), "");
+        progressCallbackFunc(RESOURCE_MANAGER.getResource("ProvisionWeb_CreatingWeb"), "");
         const createWebResult = await CreateWeb(project.Title, project.Url, project.Description, _spPageContextInfo.webLanguage, project.InheritPermissions);
         const webProperties = await GetAllProperties();
-        onUpdateProgress(RESOURCE_MANAGER.getResource("ProvisionWeb_ApplyingTemplate"), "");
-        await ApplyProvisioningTemplate(createWebResult.web, webProperties.get_fieldValues(), onUpdateProgress);
-        await ApplyExtensions(createWebResult.web, onUpdateProgress);
-        await CopyDefaultData(createWebResult.url, project.IncludeContent, onUpdateProgress);
+        progressCallbackFunc(RESOURCE_MANAGER.getResource("ProvisionWeb_ApplyingTemplate"), "");
+        await ApplyProvisioningTemplate(createWebResult.web, webProperties.get_fieldValues(), progressCallbackFunc);
+        await ApplyExtensions(createWebResult.web, progressCallbackFunc);
+        await CopyDefaultData(createWebResult.url, project.IncludeContent, progressCallbackFunc);
         return createWebResult.redirectUrl;
     } catch (err) {
-        let Message, ErrorTraceCorrelationId, ErrorTypeName;
-        if (err.hasOwnProperty("sender") && err.hasOwnProperty("args")) {
-            const { args } = err;
-            ErrorTraceCorrelationId = args.get_errorTraceCorrelationId();
-            ErrorTypeName = args.get_errorTypeName();
-            Message = args.get_message();
-        } else {
-            Message = err;
-        }
-        __listLogger.log({ Message, LogLevel: LogLevel.Error, ErrorTraceCorrelationId, ErrorTypeName, LogURL: project.Url });
+        const logEntry: ILogEntry = {
+            ...err,
+            LogURL: project.Url,
+            LogLevel: LogLevel.Error,
+        };
+        __listLogger.log(logEntry);
+        throw err;
     }
 }
 
