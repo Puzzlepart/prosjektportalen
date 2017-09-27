79de74a9ff7b63d7247ad4219f146d7294292809
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-09-27 21:09:07 +0200

    Adding pp_datasourcesiteurl to property bag

--- src/js/Provision/IProvisionContext.ts
+++ src/js/Provision/IProvisionContext.ts
@@ -6,5 +6,5 @@ export default interface IProvisionContext {
     model: IProjectModel;
     progressCallbackFunc: IProgressCallback;
     webCreationResult?: ICreateWebResult;
-    webProperties?: { pp_assetssiteurl: string, pp_version: string };
+    webProperties?: { pp_assetssiteurl: string, pp_datasourcesiteurl: string, pp_version: string };
 }
