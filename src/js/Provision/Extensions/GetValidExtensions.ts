f351ec5de29825d4453034569e03c22770e6ac8d
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-09-29 12:50:06 +0200

    Some more fixes to extensions

--- src/js/Provision/Extensions/GetValidExtensions.ts
+++ src/js/Provision/Extensions/GetValidExtensions.ts
@@ -12,12 +12,8 @@ async function GetValidExtensions(extensionLibTitle = RESOURCE_MANAGER.getResour
     const rootWeb = new Site(_spPageContextInfo.siteAbsoluteUrl).rootWeb;
     const extensionLib = rootWeb.lists.getByTitle(extensionLibTitle);
     try {
-        const extensionFiles = await extensionLib.items
-            .select("Title", "LinkFilename", "FileRef")
-            .filter("ExtensionEnabled eq 1")
-            .orderBy("ExtensionOrder")
-            .get();
-        const extensions: any[] = await Promise.all(extensionFiles.map(file => LoadExtension(file)));
+        const files = await extensionLib.items.select("Title", "LinkFilename", "FileRef").filter("GtIsEnabled eq 1").orderBy("GtOrder").get();
+        const extensions: any[] = await Promise.all(files.map(file => LoadExtension(file)));
         const validExtensions = extensions.filter(ext => ext.isValid);
         return validExtensions;
     } catch (err) {
