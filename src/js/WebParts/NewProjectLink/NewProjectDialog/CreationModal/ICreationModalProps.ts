46e5501a1e32cff0a752242d52929266f8be609f
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-10-06 16:22:41 +0200

    NewProjectLink

--- src/js/WebParts/NewProjectLink/NewProjectDialog/CreationModal/ICreationModalProps.ts
+++
@@ -1,9 +0,0 @@
-interface ICreationModalProps {
-    isBlocking: boolean;
-    isDarkOverlay?: boolean;
-    title: string;
-    progressLabel: string;
-    progressDescription: string;
-}
-
-export default ICreationModalProps;
