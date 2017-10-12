ca810c9ca44716924e6975143a076ac13cd1d4b5
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-10-12 14:25:05 +0200

    Minor edit

--- .tasks/default.js
+++ .tasks/default.js
@@ -2,6 +2,6 @@
 var gulp = require("gulp"),
     runSequence = require("run-sequence");
 
-gulp.task(`default`, (done) => {
+gulp.task("default", (done) => {
     runSequence("clean", "buildJsonResources", "tsLint", "packageProd", "buildPnpTemplateFiles", done)
 });
