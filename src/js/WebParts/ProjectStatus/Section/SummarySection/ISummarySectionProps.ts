8b1408094963309dc1e266fb548fa1a56be62430
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-10-19 10:42:38 +0200

    SummarySection: Support for style, title and titleUrl

--- src/js/WebParts/ProjectStatus/Section/SummarySection/ISummarySectionProps.ts
+++ src/js/WebParts/ProjectStatus/Section/SummarySection/ISummarySectionProps.ts
@@ -1,7 +1,9 @@
 import SectionModel from "../SectionModel";
 
-export default interface ISummarySectionProps {
+export default interface ISummarySectionProps extends React.HTMLAttributes<HTMLElement> {
     webUrl?: string;
+    title?: string;
+    titleUrl?: string;
     project: any;
     sections: SectionModel[];
 }
