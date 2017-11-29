c6c2d3ee54e3a1c252613f320637c7591b3290bc
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-11-29 12:31:53 +0100

    NewProjectFormSettingsSection: Using component ToggleSection

--- src/js/WebParts/NewProjectForm/NewProjectFormSettingsSection.tsx
+++ src/js/WebParts/NewProjectForm/NewProjectFormSettingsSection.tsx
@@ -7,6 +7,72 @@ import ListConfig from "../../Provision/Data/Config/ListConfig";
 import Extension from "../../Provision/Extensions/Extension";
 //#endregion
 
+export interface IToggleSectionProps extends React.HTMLAttributes<HTMLElement> {
+    headerClassName?: string;
+    options: any[];
+    optLabelProp: string;
+    optDefaultCheckedProp: string;
+    toggleOptionHandler: (opt, checked: boolean) => void;
+}
+
+export interface IToggleSectionState {
+    isExpanded: boolean;
+}
+
+export class ToggleSection extends React.Component<IToggleSectionProps, IToggleSectionState> {
+    public static defaultProps = {
+        headerClassName: "ms-font-l toggle-section",
+    };
+
+    /**
+    * Constructor
+    *
+    * @param {IToggleSectionProps} props Props
+    */
+    constructor(props: IToggleSectionProps) {
+        super(props);
+        this.state = { isExpanded: false };
+        this._onToggle = this._onToggle.bind(this);
+    }
+
+    public render() {
+        return (
+            <div hidden={this.props.hidden}>
+                <div onClick={this._onToggle} className={this.props.headerClassName}>
+                    <span>{this.props.title}</span>
+                    <span className={this.state.isExpanded ? "ChevronUp" : "ChevronDown"}>
+                        <Icon iconName={this.state.isExpanded ? "ChevronUp" : "ChevronDown"} />
+                    </span>
+                </div>
+                <section hidden={!this.state.isExpanded}>
+                    {this.props.options.map((opt, index) => {
+                        return (
+                            <div key={index}>
+                                <Toggle
+                                    defaultChecked={opt[this.props.optDefaultCheckedProp] === true}
+                                    label={opt[this.props.optLabelProp]}
+                                    onChanged={checked => this.props.toggleOptionHandler(opt, checked)}
+                                    onText={RESOURCE_MANAGER.getResource("String_Yes")}
+                                    offText={RESOURCE_MANAGER.getResource("String_No")} />
+                                <div className="ms-font-xs" style={{ paddingTop: 10, paddingBottom: 10 }} hidden={!opt.Comments}>
+                                    {opt.Comments}
+                                </div>
+                            </div>
+                        );
+                    })}
+                </section>
+            </div>
+        );
+    }
+
+    /**
+     * On toggle
+     */
+    private _onToggle() {
+        this.setState(prevState => ({ isExpanded: !prevState.isExpanded }));
+    }
+}
+
 export interface INewProjectFormSettingsSectionProps {
     className: string;
     toggleSectionClassName?: string;
@@ -16,10 +82,7 @@ export interface INewProjectFormSettingsSectionProps {
     toggleExtensionHandler: (extension: Extension, checked: boolean) => void;
 }
 
-export interface INewProjectFormSettingsSectionState {
-    showListContentSettings: boolean;
-    showExtensionSettings: boolean;
-}
+export interface INewProjectFormSettingsSectionState { }
 
 export default class NewProjectFormSettingsSection extends React.Component<INewProjectFormSettingsSectionProps, INewProjectFormSettingsSectionState> {
     public static defaultProps = {
@@ -33,65 +96,26 @@ export default class NewProjectFormSettingsSection extends React.Component<INewP
     */
     constructor(props: INewProjectFormSettingsSectionProps) {
         super(props);
-        this.state = {
-            showListContentSettings: false,
-            showExtensionSettings: false,
-        };
+        this.state = {};
     }
 
     public render() {
-        const { className, toggleSectionClassName, listData, extensions, toggleListContentHandler, toggleExtensionHandler } = this.props;
-        const { showListContentSettings, showExtensionSettings } = this.state;
-
-        const listDataKeys = Object.keys(listData);
-
         return (
-            <div className={className}>
-                <div hidden={listDataKeys.length === 0}>
-                    <div onClick={e => this.setState({ showListContentSettings: !showListContentSettings })} className={toggleSectionClassName}>
-                        <span>{RESOURCE_MANAGER.getResource("NewProjectForm_ShowListContentSettings")}</span>
-                        <span className={showListContentSettings ? "ChevronUp" : "ChevronDown"}>
-                            <Icon iconName={showListContentSettings ? "ChevronUp" : "ChevronDown"} />
-                        </span>
-                    </div>
-                    <section hidden={!showListContentSettings}>
-                        {listData.map((lc, index) => (
-                            <Toggle
-                                key={`ListData_${index}`}
-                                defaultChecked={lc.Default}
-                                label={lc.Label}
-                                onChanged={checked => toggleListContentHandler(lc, checked)}
-                                onText={RESOURCE_MANAGER.getResource("String_Yes")}
-                                offText={RESOURCE_MANAGER.getResource("String_No")} />
-                        ))}
-                    </section>
-                </div>
-                <div hidden={extensions.length === 0}>
-                    <div onClick={e => this.setState({ showExtensionSettings: !showExtensionSettings })} className={toggleSectionClassName}>
-                        <span>{RESOURCE_MANAGER.getResource("NewProjectForm_ShowExtensionSettings")}</span>
-                        <span className={showExtensionSettings ? "ChevronUp" : "ChevronDown"}>
-                            <Icon iconName={showExtensionSettings ? "ChevronUp" : "ChevronDown"} />
-                        </span>
-                    </div>
-                    <section hidden={!showExtensionSettings}>
-                        {extensions.map((extension, index) => {
-                            return (
-                                <div>
-                                    <Toggle
-                                        key={`Extension_${index}`}
-                                        defaultChecked={extension.IsEnabled}
-                                        label={extension.Title}
-                                        onChanged={checked => toggleExtensionHandler(extension, checked)}
-                                        onText={RESOURCE_MANAGER.getResource("String_Yes")}
-                                        offText={RESOURCE_MANAGER.getResource("String_No")} />
-                                    <div className="ms-font-xs" style={{ paddingTop: 10, paddingBottom: 10 }} hidden={!extension.Comments}>
-                                        {extension.Comments}
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </section>
-                </div>
+            <div className={this.props.className}>
+                <ToggleSection
+                    title={RESOURCE_MANAGER.getResource("NewProjectForm_ShowListContentSettings")}
+                    options={this.props.listData}
+                    optLabelProp="Label"
+                    optDefaultCheckedProp="Default"
+                    toggleOptionHandler={this.props.toggleListContentHandler}
+                    hidden={this.props.listData.length === 0} />
+                <ToggleSection
+                    title={RESOURCE_MANAGER.getResource("NewProjectForm_ShowExtensionSettings")}
+                    options={this.props.extensions}
+                    optLabelProp="Title"
+                    optDefaultCheckedProp="IsEnabled"
+                    toggleOptionHandler={this.props.toggleExtensionHandler}
+                    hidden={this.props.extensions.length === 0} />
             </div>
         );
     }
