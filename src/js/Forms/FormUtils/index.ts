6b10510bf4de729c6f0554c986025874328bc166
Ole Martin Pettersen <olemp@puzzlepart.com>
2018-01-31 11:46:45 +0100

    Fixed FormUtils type error in getQueryParams

--- src/js/Forms/FormUtils/index.ts
+++ src/js/Forms/FormUtils/index.ts
@@ -1,37 +1,27 @@
-import * as  querystring from "querystring";
-
-/**
- * Query Params
- */
-export interface IQueryParams {
-    HideWebPartMaintenancePageLink: string;
-    HideContentTypeChoice: string;
-    HideFormFields: string;
-    HideViewSelector: string;
-}
+import * as queryString from "querystring";
 
 /**
  * Get query parameters using querystring
  */
-export const getQueryParams = (): IQueryParams => {
-    return querystring.parse(document.location.search.substring(1));
-};
+export function getQueryParams(): queryString.ParsedUrlQuery {
+    return queryString.parse(document.location.search.substring(1));
+}
 
 /**
  * Hides field in form
  *
  * @param {string} fieldName Field name
  */
-export const hideFormField = (fieldName: string): void => {
+export function hideFormField(fieldName: string): void {
     (document.querySelector(`input[id*='${fieldName}_'], select[id*='${fieldName}_'], div[id*='${fieldName}_']`) as any).parentNode.parentNode.style.display = "none";
-};
+}
 
 /**
  * Hides content type choice
  */
-export const hideContentTypeChoice = (): void => {
+export function hideContentTypeChoice(): void {
     (document.querySelector(`select[id*='ContentTypeChoice']`) as any).parentNode.parentNode.style.display = "none";
-};
+}
 
 /**
  * Inserts form container with the specified ID
@@ -39,20 +29,20 @@ export const hideContentTypeChoice = (): void => {
  * @param {string} id ID of form container
  * @param {string} refNodeId Reference node ID
  */
-export const insertFormContainer = (id: string, refNodeId = "WebPartWPQ1"): HTMLElement => {
+export function insertFormContainer(id: string, refNodeId = "WebPartWPQ1"): HTMLElement {
     let refNode = document.getElementById(refNodeId),
         newNode = document.createElement("div");
     newNode.id = id;
     refNode.parentNode.insertBefore(newNode, refNode.nextSibling);
     return document.getElementById(id);
-};
+}
 
 /**
  * Overrides pre save action with the specified function
  *
  * @param {Function} func Override function
  */
-export const overridePreSaveAction = (func): void => {
+export function overridePreSaveAction(func): void {
     let f = "PreSaveAction";
     window[f] = func;
-};
+}
