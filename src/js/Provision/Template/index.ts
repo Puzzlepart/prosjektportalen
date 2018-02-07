50bb84841c3ba01161e663e934d6170b5897ffee
Ole Martin Pettersen <olemp@puzzlepart.com>
2018-02-07 10:25:37 +0100

    Provision\Template: Added function GetDefaultTemplate

--- src/js/Provision/Template/index.ts
+++ src/js/Provision/Template/index.ts
@@ -2,34 +2,52 @@ import RESOURCE_MANAGER from "../../@localization";
 import { WebProvisioner } from "sp-pnp-provisioning/lib/webprovisioner";
 import IProvisionContext from "../IProvisionContext";
 import ProvisionError from "../ProvisionError";
-import FullTemplate from "./FullTemplate";
+import IProvisionTemplate from "../IProvisionTemplate";
+import ApplyTemplateProgressMap from "./ApplyTemplateProgressMap";
 
 /**
- * Maps the current handler to a text explaining the current handlers action
+ * Get default template from templates library
+ *
+ * @param {IProvisionContext} context Provisioning context
  */
-const PROGRESS_MAP = {
-    Files: RESOURCE_MANAGER.getResource("ProvisionWeb_Progress_Handler_Files"),
-    Lists: RESOURCE_MANAGER.getResource("ProvisionWeb_Progress_Handler_Lists"),
-    Navigation: RESOURCE_MANAGER.getResource("ProvisionWeb_Progress_Handler_Navigation"),
-    WebSettings: RESOURCE_MANAGER.getResource("ProvisionWeb_Progress_Handler_WebSettings"),
-    ComposedLook: RESOURCE_MANAGER.getResource("ProvisionWeb_Progress_Handler_ComposedLook"),
-    PropertyBagEntries: RESOURCE_MANAGER.getResource("ProvisionWeb_Progress_Handler_PropertyBagEntries"),
-};
+export async function GetDefaultTemplate(context: IProvisionContext): Promise<IProvisionTemplate> {
+    const templatesLib = context.rootWeb.lists.getByTitle(RESOURCE_MANAGER.getResource("Lists_Templates_Title"));
+    try {
+        const [defaultTemplate] = await templatesLib
+            .items
+            .select("Title", "LinkFilename", "FileRef")
+            .filter("GtIsDefault eq 1")
+            .orderBy("GtOrder")
+            .top(1)
+            .get();
+        if (defaultTemplate) {
+            const fileRef = defaultTemplate.FileRef;
+            const defaultTemplateFile = context.rootWeb.getFileByServerRelativeUrl(fileRef);
+            const defaultTemplateFileText = await defaultTemplateFile.getText();
+            const defaultTemplateFileJson = JSON.parse(defaultTemplateFileText);
+            return { fileRef, schema: defaultTemplateFileJson };
+        } else {
+            throw new ProvisionError("No default template found.", "GetDefaultTemplate");
+        }
+    } catch (err) {
+        throw new ProvisionError(err, "GetDefaultTemplate");
+    }
+}
 
 /**
  * Applies the template to the specified web
  *
  * @param {IProvisionContext} context Provisioning context
  */
-export async function ApplyProvisioningTemplate(context: IProvisionContext): Promise<void> {
+export async function ApplyTemplate(context: IProvisionContext): Promise<void> {
     context.progressCallbackFunc(RESOURCE_MANAGER.getResource("ProvisionWeb_ApplyingTemplate"), "");
     const webProvisioner = new WebProvisioner(context.web);
-    const callbackFunc = objHandler => context.progressCallbackFunc(RESOURCE_MANAGER.getResource("ProvisionWeb_ApplyingTemplate"), PROGRESS_MAP[objHandler]);
+    const callbackFunc = objHandler => context.progressCallbackFunc(RESOURCE_MANAGER.getResource("ProvisionWeb_ApplyingTemplate"), ApplyTemplateProgressMap[objHandler]);
     try {
         const template = {
-            ...FullTemplate,
+            ...context.template.schema,
             WebSettings: {
-                ...FullTemplate.WebSettings,
+                ...context.template.schema.WebSettings,
                 AlternateCssUrl: `${context.webProperties.pp_assetssiteurl}/SiteAssets/pp/css/pp.main.css`,
                 SiteLogoUrl: `${context.webProperties.pp_assetssiteurl}/SiteAssets/pp/img/ICO-Site-Project-11.png`,
             },
@@ -43,7 +61,7 @@ export async function ApplyProvisioningTemplate(context: IProvisionContext): Pro
         await webProvisioner.applyTemplate(template, callbackFunc);
         return;
     } catch (err) {
-        throw new ProvisionError(err, "ApplyProvisioningTemplate");
+        throw new ProvisionError(err, "ApplyTemplate");
     }
 }
 
