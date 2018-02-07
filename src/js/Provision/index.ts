352aab7d497be768a996fbffe46275d5da13732b
Ole Martin Pettersen <olemp@puzzlepart.com>
2018-02-07 10:26:30 +0100

    Provision\ProvisionWeb: Added another step, GetDefaultTemplate

--- src/js/Provision/index.ts
+++ src/js/Provision/index.ts
@@ -2,7 +2,7 @@ import { Site } from "sp-pnp-js";
 import { CreateWeb, DoesWebExist } from "./Subsite";
 import { IProjectModel } from "../Model/ProjectModel";
 import { CopyDefaultData } from "./Data";
-import { ApplyProvisioningTemplate } from "./Template";
+import { ApplyTemplate, GetDefaultTemplate } from "./Template";
 import { ApplyExtensions } from "./Extensions";
 import { GetAllProperties } from "../Util/PropertyBag";
 import SpListLogger, { LogLevel, ILogEntry } from "../Util/SpListLogger";
@@ -14,25 +14,23 @@ import IProvisionContext from "./IProvisionContext";
  *
  * @param {IProjectModel} model The project model
  * @param {IProgressCallback} progressCallbackFunc Progress callback function
+ * @param {string} templateFileRef Template file reference (site relative URL)
  *
  * @returns {string} Redirect URL
  */
-export default async function ProvisionWeb(project: IProjectModel, progressCallbackFunc: IProgressCallback): Promise<string> {
+export default async function ProvisionWeb(model: IProjectModel, progressCallbackFunc: IProgressCallback, templateFileRef?: string): Promise<string> {
     try {
         const rootWeb = new Site(_spPageContextInfo.siteAbsoluteUrl).rootWeb;
-        let context: IProvisionContext = {
-            model: project,
-            progressCallbackFunc,
-            rootWeb,
-        };
+        let context: IProvisionContext = { model, progressCallbackFunc, rootWeb, template: { fileRef: templateFileRef } };
         context = await CreateWeb(context);
         context.webProperties = await GetAllProperties();
-        await ApplyProvisioningTemplate(context);
+        context.template = await GetDefaultTemplate(context);
+        await ApplyTemplate(context);
         await ApplyExtensions(context);
         await CopyDefaultData(context);
         return context.redirectUrl;
     } catch (err) {
-        const logEntry: ILogEntry = { ...err, LogURL: project.Url, LogLevel: LogLevel.Error };
+        const logEntry: ILogEntry = { ...err, LogURL: model.Url, LogLevel: LogLevel.Error };
         new SpListLogger().log(logEntry);
         throw err;
     }
