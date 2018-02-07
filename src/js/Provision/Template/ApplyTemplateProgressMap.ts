479d69eeefdd82a9eec5bd5485e7794f9cf20d37
Ole Martin Pettersen <olemp@puzzlepart.com>
2018-02-07 10:26:01 +0100

    Separated ApplyTemplateProgressMap in its own file

---
+++ src/js/Provision/Template/ApplyTemplateProgressMap.ts
@@ -0,0 +1,12 @@
+import RESOURCE_MANAGER from "../../@localization";
+
+const ApplyTemplateProgressMap = {
+    Files: RESOURCE_MANAGER.getResource("ProvisionWeb_Progress_Handler_Files"),
+    Lists: RESOURCE_MANAGER.getResource("ProvisionWeb_Progress_Handler_Lists"),
+    Navigation: RESOURCE_MANAGER.getResource("ProvisionWeb_Progress_Handler_Navigation"),
+    WebSettings: RESOURCE_MANAGER.getResource("ProvisionWeb_Progress_Handler_WebSettings"),
+    ComposedLook: RESOURCE_MANAGER.getResource("ProvisionWeb_Progress_Handler_ComposedLook"),
+    PropertyBagEntries: RESOURCE_MANAGER.getResource("ProvisionWeb_Progress_Handler_PropertyBagEntries"),
+};
+
+export default ApplyTemplateProgressMap;
