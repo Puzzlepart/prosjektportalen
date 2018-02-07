03e50b7335559a0df2a124d0faa0efa822c390dc
Ole Martin Pettersen <olemp@puzzlepart.com>
2018-02-07 12:24:06 +0100

    Fixed build/bundle

--- .tasks/package.js
+++ .tasks/package.js
@@ -9,52 +9,43 @@ var gulp = require("gulp"),
     settings = require('./@settings.js'),
     config = require('./@configuration.js');
 
-gulp.task("packageCode", ["buildLib"], (done) => {
+gulp.task("packageCode", ["buildLib"], done => {
     webpack(wpDev("source-map"), (err, stats) => {
         if (err) {
             throw new pluginError("packageCode", err);
         }
-        console.log(stats.toString({
-            colors: true
-        }));
         done();
     });
 });
 
-gulp.task("packageCodeEval", ["buildLib"], (done) => {
+gulp.task("packageCodeEval", ["buildLib"], done => {
     webpack(wpDev("eval"), (err, stats) => {
         if (err) {
             throw new pluginError("packageCodeEval", err);
         }
-        console.log(stats.toString({
-            colors: true
-        }));
         done();
     });
 });
 
-gulp.task("packageStyles", ["buildTheme"], (done) => {
+gulp.task("packageStyles", ["buildTheme"], done => {
     return gulp.src(config.paths.stylesMain)
         .pipe(stylus(config.stylus))
         .pipe(gulp.dest(config.paths.dist));
 });
 
-gulp.task("package", ["copyAssetsToDist", "packageCode", "packageStyles"], (done) => {
+gulp.task("package", ["copyAssetsToDist", "packageCode", "packageStyles"], done => {
     done();
 });
 
-gulp.task("packageCodeMinify", ["buildLib"], (done) => {
+gulp.task("packageCodeMinify", ["buildLib"], done => {
     webpack(wpProd(), (err, stats) => {
         if (err) {
             throw new pluginError("packageCodeMinify", err);
         }
-        console.log(stats.toString({
-            colors: true
-        }));
         done();
     });
 });
 
-gulp.task("packageProd", ["copyAssetsToDist", "packageCodeMinify", "packageStyles"], (done) => {
+gulp.task("packageProd", ["copyAssetsToDist", "packageCodeMinify", "packageStyles"], done => {
     done();
 });
