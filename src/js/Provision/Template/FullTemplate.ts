fcd517a13dd00f6dddc4ea3efa7f511fc9cde37b
Ole Martin Pettersen <olemp@puzzlepart.com>
2018-02-09 12:13:03 +0100

    More Node support

--- src/js/Provision/Template/FullTemplate.ts
+++ src/js/Provision/Template/FullTemplate.ts
@@ -1,9 +1,23 @@
 import { Schema } from "sp-pnp-provisioning/lib/schema";
-import { Files } from "./Objects";
+import {
+    Files,
+    Lists,
+    Navigation,
+    WebSettings,
+    ComposedLook,
+} from "./Objects";
 
-const FullTemplate: Schema = {
-    Files: Files(1044),
-};
-
-export default FullTemplate;
-
+export default function FullTemplate(language: number): Schema {
+    return {
+        Files: Files(language),
+        Lists: Lists(language),
+        Navigation: Navigation(language),
+        WebSettings,
+        ComposedLook,
+        Features: [{
+            id: "87294c72-f260-42f3-a41b-981a2ffce37a",
+            deactivate: true,
+            force: true,
+        }],
+    };
+}
