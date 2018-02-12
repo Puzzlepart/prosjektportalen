6a63a751153393dcae21dc4da5ddd2d295f9dd74
Ole Martin Pettersen <olemp@puzzlepart.com>
2018-02-12 14:46:15 +0100

    Updated LiteTemplate - Removed list from ProjectHome

--- src/js/Provision/Template/_/LiteTemplate.ts
+++ src/js/Provision/Template/_/LiteTemplate.ts
@@ -1,6 +1,6 @@
 import RESOURCE_MANAGER from "../../../@localization";
 import { Schema } from "sp-pnp-provisioning/lib/schema";
-import { Files, WebSettings, ComposedLook } from "../Objects";
+import { WebSettings, ComposedLook } from "../Objects";
 import SitePages from "../Objects/Lists/SitePages";
 import PhaseChecklist from "../Objects/Lists/PhaseChecklist";
 import Information from "../Objects/Lists/Information";
@@ -14,11 +14,171 @@ import MeetingCalendar from "../Objects/Lists/MeetingCalendar";
 import Documents from "../Objects/Lists/Documents";
 import ProjectStatus from "../Objects/Lists/ProjectStatus";
 import { GtProjectTaskComElement, GtProjectTaskRisk, GtProjectTaskProduct } from "../Objects/Lists/SiteFields";
+import { default as ProjectStatusPage } from "../Objects/Files/ProjectStatus";
+import { default as NofilterPage } from "../Objects/Files/Nofilter";
+import { default as AssignedPage } from "../Objects/Files/Assigned";
+import { default as MoMPage } from "../Objects/Files/MoM";
 
 
 export default function LiteTemplate(language: number): Schema {
     return {
-        Files,
+        Files: [
+            {
+                Folder: "SitePages",
+                Src: "{sitecollection}/Resources/SitePage.txt",
+                Url: "ProjectHome.aspx",
+                Overwrite: true,
+                Properties: {
+                    Title: "Hjemmeside",
+                    ContentTypeId: "0x010109010058561F86D956412B9DD7957BBCD67AAE01",
+                },
+                RemoveExistingWebParts: true,
+                WebParts: [
+                    {
+                        Title: RESOURCE_MANAGER.getResource("WebPart_ProjectPhases_Title"),
+                        Zone: "LeftColumn",
+                        Order: 0,
+                        Contents: {
+                            FileSrc: "{wpgallery}/ProjectPhases.webpart",
+                        },
+                    },
+                    {
+                        Title: RESOURCE_MANAGER.getResource("WebPart_Timeline_Title"),
+                        Zone: "LeftColumn",
+                        Order: 1,
+                        PropertyOverrides: [{
+                            name: "Title",
+                            type: "string",
+                            value: RESOURCE_MANAGER.getResource("WebPart_Timeline_Title"),
+                        },
+                        {
+                            name: "ListUrl",
+                            type: "string",
+                            value: `{site}/${RESOURCE_MANAGER.getResource("Lists_Tasks_Url")}`,
+                        },
+                        {
+                            name: "TitleUrl",
+                            type: "string",
+                            value: `{site}/${RESOURCE_MANAGER.getResource("DefaultView_Tasks_Url")}`,
+                        },
+                        {
+                            name: "CurrentTaskListWebAddress",
+                            type: "string",
+                            value: "{site}",
+                        },
+                        {
+                            name: "PageAddress",
+                            type: "string",
+                            value: "{site}",
+                        },
+                        {
+                            name: "ViewName",
+                            type: "string",
+                            value: RESOURCE_MANAGER.getResource("WebPart_Timeline_ViewName"),
+                        },
+                        {
+                            name: "TimelineType",
+                            type: "string",
+                            value: "TaskListTimeline",
+                        }],
+                        Contents: {
+                            FileSrc: "{wpgallery}/Timeline.webpart",
+                        },
+                    },
+                    {
+                        Title: RESOURCE_MANAGER.getResource("WebPart_DocumentsCurrentPhase_Title"),
+                        Zone: "LeftColumn",
+                        Order: 2,
+                        PropertyOverrides: [{
+                            name: "Title",
+                            type: "string",
+                            value: RESOURCE_MANAGER.getResource("WebPart_DocumentsCurrentPhase_Title"),
+                        },
+                        {
+                            name: "ListUrl",
+                            type: "string",
+                            value: `{site}/${RESOURCE_MANAGER.getResource("Lists_Documents_Url")}`,
+                        },
+                        {
+                            name: "TitleUrl",
+                            type: "string",
+                            value: `{site}/${RESOURCE_MANAGER.getResource("DefaultView_Documents_Url")}`,
+                        }],
+                        Contents: {
+                            FileSrc: "{sitecollection}/Resources/ListViewWebPart.txt",
+                        },
+                        ListView: {
+                            List: RESOURCE_MANAGER.getResource("Lists_Documents_Title"),
+                            View: {
+                                Title: "",
+                                ViewFields: ["DocIcon", "LinkFilename", "Modified", "Editor"],
+                                AdditionalSettings: {
+                                    RowLimit: 10,
+                                    Paged: true,
+                                    ViewQuery: "<OrderBy><FieldRef Name='Created' Ascending='FALSE' /></OrderBy>",
+                                    Scope: 0,
+                                },
+                            },
+                        },
+                    },
+                    {
+                        Title: RESOURCE_MANAGER.getResource("WebPart_ProjectInfo_Title"),
+                        Zone: "RightColumn",
+                        Order: 0,
+                        Contents: {
+                            FileSrc: "{wpgallery}/ProjectInfo.webpart",
+                        },
+                    },
+                    {
+                        Title: RESOURCE_MANAGER.getResource("WebPart_SiteFeed_Title"),
+                        Zone: "RightColumn",
+                        Order: 1,
+                        Contents: {
+                            FileSrc: "{wpgallery}/SiteFeed.dwp",
+                        },
+                    },
+                    {
+                        Title: RESOURCE_MANAGER.getResource("WebPart_UncertaintiesCurrentPhase_Title"),
+                        Zone: "RightColumn",
+                        Order: 2,
+                        PropertyOverrides: [{
+                            name: "Title",
+                            type: "string",
+                            value: RESOURCE_MANAGER.getResource("WebPart_UncertaintiesCurrentPhase_Title"),
+                        },
+                        {
+                            name: "ListUrl",
+                            type: "string",
+                            value: `{site}/${RESOURCE_MANAGER.getResource("Lists_Uncertainties_Url")}`,
+                        },
+                        {
+                            name: "TitleUrl",
+                            type: "string",
+                            value: `{site}/${RESOURCE_MANAGER.getResource("DefaultView_Uncertainties_Url")}`,
+                        }],
+                        Contents: {
+                            FileSrc: "{sitecollection}/Resources/ListViewWebPart.txt",
+                        },
+                        ListView: {
+                            List: RESOURCE_MANAGER.getResource("Lists_Uncertainties_Title"),
+                            View: {
+                                Title: "",
+                                ViewFields: ["LinkTitle", "GtRiskProximity", "GtRiskFactor"],
+                                AdditionalSettings: {
+                                    RowLimit: 10,
+                                    Paged: true,
+                                    ViewQuery: "<OrderBy><FieldRef Name='Created' Ascending='FALSE' /></OrderBy>",
+                                },
+                            },
+                        },
+                    },
+                ],
+            },
+            NofilterPage,
+            AssignedPage,
+            MoMPage,
+            ProjectStatusPage,
+        ],
         Lists: [
             SitePages,
             PhaseChecklist,
