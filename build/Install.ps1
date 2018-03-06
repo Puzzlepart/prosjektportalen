a4cbff85806a48401ff987597ecbbefd6cae9a06
Ole Martin Pettersen <olemp@puzzlepart.com>
2018-03-06 15:17:34 +0100

    Removed duplicate Write-Host

--- build/Install.ps1
+++ build/Install.ps1
@@ -133,7 +133,6 @@ function Start-Install() {
     if (-not $SkipRootPackage.IsPresent) {
         try {
             Connect-SharePoint $Url    
-            Write-Host "Deploying root-package with fields, content types, lists and pages..." -ForegroundColor Green -NoNewLine
             if (-not $Upgrade.IsPresent) {
                 Write-Host "Deploying root-package with fields, content types, lists, navigation and pages..." -ForegroundColor Green -NoNewLine
                 Apply-Template -Template "root" -Localized -ExcludeHandlers PropertyBagEntries -Parameters $Parameters
