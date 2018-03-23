89767058c62e556a2f92269ba1cf3ca205e45028
olemp@puzzlepart.com <olemp@puzzlepart.com>
2018-03-23 12:26:28 +0100

    IContentType

---
+++ src/js/Model/IContentType.ts
@@ -0,0 +1,23 @@
+export default interface IContentType {
+    Description: string;
+    DisplayFormTemplateName: string;
+    DisplayFormUrl: string;
+    DocumentTemplate: string;
+    DocumentTemplateUrl: string;
+    EditFormTemplateName: string;
+    EditFormUrl: string;
+    Group: string;
+    Hidden: boolean;
+    JSLink: string;
+    MobileDisplayFormUrl: string;
+    MobileEditFormUrl: string;
+    MobileNewFormUrl: string;
+    Name: string;
+    NewFormTemplateName: string;
+    NewFormUrl: string;
+    ReadOnly: boolean;
+    SchemaXml: string;
+    Scope: string;
+    Sealed: boolean;
+    StringId: string;
+}
