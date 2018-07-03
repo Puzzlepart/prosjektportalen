64bd616809f4ba93b1cd8a6fa3d859ee8aab18f3
olemp@puzzlepart.com <olemp@puzzlepart.com>
2018-07-03 12:01:00 +0200

    ProvisionError: Attempting to JSON.stringify error if object

--- src/js/Provision/ProvisionError.ts
+++ src/js/Provision/ProvisionError.ts
@@ -4,17 +4,35 @@ export default class ProvisionError {
     public errorTraceCorrelationId: string;
     public errorTypeName: string;
 
+    /**
+     * Constructor
+     *
+     * @param {any} err Error
+     * @param {string} source The source of the errror
+     */
     constructor(err: any, source: string) {
         this.parseError(err);
         this.errorTypeName = source;
     }
 
-    private parseError(err) {
+    /**
+     * Parse error
+     *
+     * @param {any} err Error
+     */
+    private parseError(err: any) {
         if (err.hasOwnProperty("sender") && err.hasOwnProperty("args")) {
             const { args } = err;
             this.errorTraceCorrelationId = args.get_errorTraceCorrelationId();
             this.errorTypeName = args.get_errorTypeName();
             this.message = args.get_message();
+        } else if (typeof err === "object") {
+            try {
+                const errString = JSON.stringify(err);
+                this.message = errString;
+            } catch (e) {
+                this.message = "Unable to parse error details.";
+            }
         } else {
             if (typeof err === "string") {
                 this.message = err;
