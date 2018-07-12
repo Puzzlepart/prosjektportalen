df2ea6fcbca4e6e05d522c0bac356177b174839c
olemp <olemp@puzzlepart.com>
2018-07-12 14:59:04 +0200

    DeliveriesOverview: Group by SiteTitle/ProductStatus #571

--- src/js/WebParts/DeliveriesOverview/IDeliveriesOverviewProps.ts
+++ src/js/WebParts/DeliveriesOverview/IDeliveriesOverviewProps.ts
@@ -27,16 +27,11 @@ export default interface IDeliveriesOverviewProps extends IBaseWebPartProps {
 }
 
 export const DeliveriesOverviewDefaultProps: Partial<IDeliveriesOverviewProps> = {
-    showSearchBox: false,
-    showEmptyMessage: false,
-    groupByOptions: [],
-    constrainMode: ConstrainMode.horizontalConstrained,
+    groupByOptions: [
+        { name: RESOURCE_MANAGER.getResource("String_Project"), key: "SiteTitle" },
+        { name: RESOURCE_MANAGER.getResource("SiteFields_GtProductStatus_DisplayName"), key: "ProductStatus" },
+    ],
     dataSource: "DELIVERIESOVERVIEW",
-    rowLimit: 100,
-    projectInfoFilterField: "GtPcPortfolioPage",
-    modalHeaderClassName: "ms-font-xxl",
-    layoutMode: DetailsListLayoutMode.fixedColumns,
-    selectionMode: SelectionMode.none,
     columns: [{
         key: "Title",
         fieldName: "Title",
@@ -50,6 +45,7 @@ export const DeliveriesOverviewDefaultProps: Partial<IDeliveriesOverviewProps> =
         name: RESOURCE_MANAGER.getResource("String_Project"),
         minWidth: 220,
         isMultiline: true,
+        isResizable: true,
     },
     {
         key: "GtProductDescriptionOWSMTXT",
@@ -57,24 +53,31 @@ export const DeliveriesOverviewDefaultProps: Partial<IDeliveriesOverviewProps> =
         name: RESOURCE_MANAGER.getResource("SiteFields_GtProductDescription_DisplayName"),
         minWidth: 220,
         isMultiline: true,
+        isResizable: true,
     },
     {
         key: "GtProductStartTimeOWSDATE",
         fieldName: "ProductStartTime",
         name: RESOURCE_MANAGER.getResource("SiteFields_GtProductStartTime_DisplayName"),
+        minWidth: 100,
         isMultiline: false,
+        isResizable: true,
     },
     {
         key: "GtProductEndTimeOWSDATE",
         fieldName: "ProductEndTime",
         name: RESOURCE_MANAGER.getResource("SiteFields_GtProductEndTime_DisplayName"),
+        minWidth: 100,
         isMultiline: false,
+        isResizable: true,
     },
     {
         key: "GtProductStatusOWSCHCS",
         fieldName: "ProductStatus",
         name: RESOURCE_MANAGER.getResource("SiteFields_GtProductStatus_DisplayName"),
+        minWidth: 100,
         isMultiline: false,
+        isResizable: true,
     },
     {
         key: "GtProductStatusCommentOWSMTXT",
@@ -82,13 +85,11 @@ export const DeliveriesOverviewDefaultProps: Partial<IDeliveriesOverviewProps> =
         name: RESOURCE_MANAGER.getResource("SiteFields_GtProductStatusComment_DisplayName"),
         minWidth: 220,
         isMultiline: true,
-    }].map(col => ({
-        ...col,
         isResizable: true,
-    })),
+    }],
     excelExportEnabled: true,
     excelExportConfig: {
-        fileName: RESOURCE_MANAGER.getResource("DynamicPortfolio_ExcelExportFileName"),
+        fileNamePrefix: RESOURCE_MANAGER.getResource("DeliveriesOverview_ExcelExportFileNamePrefix"),
         sheetName: "Sheet A",
         buttonLabel: RESOURCE_MANAGER.getResource("DynamicPortfolio_ExcelExportButtonLabel"),
         buttonIcon: "ExcelDocument",
