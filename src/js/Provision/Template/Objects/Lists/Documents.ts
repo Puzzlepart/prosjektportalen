31ad76f6701bdcc4d81982c0b4bda83cf2ec3e31
olemp <olemp@puzzlepart.com>
2018-08-08 13:53:07 +0200

    Provision\Template: ContentTypesEnabled 'false' for Documents #587

--- src/js/Provision/Template/Objects/Lists/Documents.ts
+++ src/js/Provision/Template/Objects/Lists/Documents.ts
@@ -5,14 +5,10 @@ const Documents: IList = {
     Title: __.getResource("Lists_Documents_Title"),
     Description: "",
     Template: 101,
-    ContentTypesEnabled: true,
+    ContentTypesEnabled: false,
     RemoveExistingContentTypes: true,
-    ContentTypeBindings: [{
-        ContentTypeID: "0x010100293FDE3FCADA480B9A77BBDAD7DFA28C01",
-    }],
-    AdditionalSettings: {
-        EnableVersioning: true,
-    },
+    ContentTypeBindings: [{ ContentTypeID: "0x010100293FDE3FCADA480B9A77BBDAD7DFA28C01" }],
+    AdditionalSettings: { EnableVersioning: true },
     Views: [{
         Title: __.getResource("View_AllDocuments_DisplayName"),
         ViewFields: ["DocIcon", "LinkFilename", "GtProjectPhase", "Modified", "Editor"],
