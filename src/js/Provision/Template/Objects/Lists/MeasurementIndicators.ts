712879630a9026829eefc009a7eeeb91d74b4af6
olemp <olemp@puzzlepart.com>
2019-03-18 14:54:18 +0100

    Updated template

---
+++ src/js/Provision/Template/Objects/Lists/MeasurementIndicators.ts
@@ -0,0 +1,32 @@
+import __ from "../../../../Resources";
+import { IList } from "sp-js-provisioning/lib/schema";
+import { GtGainLookup, GtGainLookup_ID } from "./SiteFields";
+
+const MeasurementIndicators: IList = {
+    Title: __.getResource("Lists_MeasurementIndicators_Title"),
+    Description: "",
+    Template: 100,
+    ContentTypesEnabled: false,
+    RemoveExistingContentTypes: true,
+    ContentTypeBindings: [{
+        ContentTypeID: "0x0100FF4E12223AF44F519AF40C441D05DED0",
+    }],
+    AdditionalSettings: {
+        EnableVersioning: true,
+    },
+    Fields: [
+        GtGainLookup,
+        GtGainLookup_ID,
+    ],
+    Views: [{
+        Title: __.getResource("View_AllItems_DisplayName"),
+        ViewFields: ["GtGainLookup", "LinkTitle", "GtStartValue", "GtDesiredValue", "GtMeasurementUnit"],
+        AdditionalSettings: {
+            RowLimit: 30,
+            Paged: true,
+            ViewQuery: "",
+        },
+    }],
+};
+
+export default MeasurementIndicators;
