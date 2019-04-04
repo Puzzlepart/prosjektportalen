e4d3615a547af0daf6e4e9a3a37b8bfe8e0f9c80
olemp <olemp@puzzlepart.com>
2019-04-04 17:35:22 +0200

    Added DataSourceService

---
+++ src/js/Services/DataSourceService.ts
@@ -0,0 +1,20 @@
+import __ from "../Resources";
+import { Site } from "@pnp/sp";
+
+export default new class DataSourceService {
+    /**
+     * Get source by name
+     *
+     * @param {string} name Name
+     * @param {string} url Url
+     */
+    public async getSourceByName(name: string, url = _spPageContextInfo.siteAbsoluteUrl): Promise<string> {
+        const list = new Site(url).rootWeb.lists.getByTitle(__.getResource("Lists_DataSources_Title"));
+        const [dataSource] = await list.items.filter(`Title eq '${name}'`).get<{ GtDpSearchQuery: string }[]>();
+        if (dataSource) {
+            return dataSource.GtDpSearchQuery;
+        } else {
+            return null;
+        }
+    }
+};
