f712ec75b06f53b27afc14f79b5563e67e70e4a5
olemp <olemp@puzzlepart.com>
2019-09-05 10:16:20 +0200

    Added sample environment file (@env.sample.js)

---
+++ .tasks/@env.sample.js
@@ -0,0 +1,7 @@
+module.exports = {
+   default: {
+       siteUrl: "https://tenant.sharepoint.com/sites/prosjektportalen",
+       username: "@tenant.onmicrosoft.com",
+       password: "",
+    }
+}
