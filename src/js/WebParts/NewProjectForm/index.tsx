2d3d0b9b39f07cb9ee87c94454131ee0083ef7f8
olemp <olemp@puzzlepart.com>
2019-10-08 09:21:52 +0200

    Removed console.log

--- src/js/WebParts/NewProjectForm/index.tsx
+++ src/js/WebParts/NewProjectForm/index.tsx
@@ -287,8 +287,6 @@ export default class NewProjectForm extends React.Component<INewProjectFormProps
             selectedTemplate = this.state.config.templates.filter(t => t.Id === projectType.templateId)[0];
         }
 
-        console.log(projectType);
-
         this.setState({ model, selectedTemplate });
     }
 
