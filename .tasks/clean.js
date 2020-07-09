1377c4151cfc7a8610a6c49baae81e046db7d7a4
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-07-09 16:51:08 +0200

    Minor syntax in tasks

--- .tasks/clean.js
+++ .tasks/clean.js
@@ -1,12 +1,21 @@
-'use strict';
-var gulp = require("gulp"),
-    clean = require('gulp-clean'),
-    config = require('./@configuration.js');
+'use strict'
+const gulp = require('gulp')
+const clean = require('gulp-clean')
+const config = require('./@configuration.js')
 
-gulp.task("cleanNodeModules", () => {
-    return gulp.src(config.paths.nodeModules, { read: false }).pipe(clean());
-});
+gulp.task('cleanNodeModules', () => {
+    return gulp.src(config.paths.nodeModules, { read: false }).pipe(clean())
+})
 
-gulp.task("clean", () => {
-    return gulp.src([config.paths.lib, config.paths.dist, config.paths.templatesTemp], { read: false }).pipe(clean());
-});
+gulp.task('clean', () => {
+    return gulp.src(
+        [
+            config.paths.lib,
+            config.paths.dist,
+            config.paths.templatesTemp
+        ],
+        {
+            read: false
+        }
+    ).pipe(clean())
+})
