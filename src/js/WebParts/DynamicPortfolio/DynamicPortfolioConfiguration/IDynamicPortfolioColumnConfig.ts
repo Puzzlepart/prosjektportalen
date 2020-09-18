263f836badfce69b2cce61635cf235c3516fd6d5
Remi <remi.blom-ohlsen@puzzlepart.com>
2020-09-18 07:46:09 +0200

    Add float and percentage formatting for crawled/managed props

--- src/js/WebParts/DynamicPortfolio/DynamicPortfolioConfiguration/IDynamicPortfolioColumnConfig.ts
+++ src/js/WebParts/DynamicPortfolio/DynamicPortfolioConfiguration/IDynamicPortfolioColumnConfig.ts
@@ -2,7 +2,7 @@ import { IColumn } from 'office-ui-fabric-react/lib/DetailsList'
 
 interface IDynamicPortfolioColumnConfig extends IColumn {
     readOnly: boolean;
-    render: 'Date' | 'Note' | 'Currency' | 'Status' | 'URL' | 'Default';
+    render: 'Date' | 'Note' | 'Percentage' | 'Float' | 'Currency' | 'Status' | 'URL' | 'Default';
     groupBy?: boolean;
 }
 
